2b2637fa57e70a7374deaf7246b57dd82ddd262a
Mohsen Vakilian <reprogrammer@gmail.com>
2010-10-20 11:24:49 -0500

    Fixed a typo in a comment.

--- org.eclipse.ltk.ui.refactoring/src/org/eclipse/ltk/ui/refactoring/RefactoringWizard.java
+++ org.eclipse.ltk.ui.refactoring/src/org/eclipse/ltk/ui/refactoring/RefactoringWizard.java
@@ -678,7 +678,7 @@ public abstract class RefactoringWizard extends Wizard {
 			// Wrap it into a refactoring descriptor proxy
 			RefactoringDescriptorProxy proxy= new RefactoringDescriptorProxyAdapter(refactoringDescriptor);
 
-			// Wrap it into a refactoringdecriptorevent using proxy
+			// Wrap it into a refactoringdecriptor event using proxy
 			RefactoringHistoryEvent event= new RefactoringHistoryEvent(RefactoringCore.getHistoryService(), refactoringEventType, proxy);
 
 			// Call RefactoringHistorySerializer to persist
