5d328cad63d695e9f3e910ff9a2b7c5140331bef
Nick Chen <nchen@illinois.edu>
2010-10-29 14:57:49 -0500

    (Amend)Implemented IWatchedRefactoring and IWatchedProcessor
    Added missing files from last commit.

---
+++ org.eclipse.ltk.core.refactoring/src/org/eclipse/ltk/core/refactoring/IWatched.java
@@ -0,0 +1,12 @@
+package org.eclipse.ltk.core.refactoring;
+
+/**
+ * @author Mohsen Vakilian
+ * @author nchen
+ * 
+ */
+public interface IWatched {
+
+	public abstract RefactoringDescriptor getSimpleRefactoringDescriptor(RefactoringStatus refactoringStatus);
+
+}
