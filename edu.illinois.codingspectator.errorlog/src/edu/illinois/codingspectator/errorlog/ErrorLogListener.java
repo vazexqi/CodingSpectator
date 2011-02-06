d8317781ba131ceda94396d58050ef5b52bb3b5f
Balaji Ambresh Rajkumar <rockmylife@gmail.com>
2011-02-06 16:13:58 -0500

    Added a comment about using EclipseLog. This commit belongs to issue #143.

--- edu.illinois.codingspectator.errorlog/src/edu/illinois/codingspectator/errorlog/ErrorLogListener.java
+++ edu.illinois.codingspectator.errorlog/src/edu/illinois/codingspectator/errorlog/ErrorLogListener.java
@@ -1,13 +1,16 @@
 /**
  * This file is licensed under the University of Illinois/NCSA Open Source License. See LICENSE.TXT for details.
  */
-
 package edu.illinois.codingspectator.errorlog;
 
 import org.eclipse.core.runtime.ILogListener;
 import org.eclipse.core.runtime.IStatus;
 
 /**
+ * TODO: We need to write the reported error into a file.
+ * 
+ * Depending on how we resolve issue #149, we might be able to reuse classes
+ * EclipseLog and PlatformLogWriter to output the errors into our own file.
  * 
  * @author Mohsen Vakilian
  * @author Balaji Ambresh Rajkumar
