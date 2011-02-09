fbe5e43136c21d73e51907c4dfde1b1d00fa5efa
Mohsen Vakilian <reprogrammer@gmail.com>
2011-02-08 18:24:28 -0600

    Renamed the test for extract super class. This commit belongs to issue #141.

---
+++ edu.illinois.codingspectator.ui.tests/src/edu/illinois/codingspectator/ui/tests/extractsuperclass/PerformedValidMultiStepExtractSuperclassTest.java
@@ -0,0 +1,62 @@
+/**
+ * This file is licensed under the University of Illinois/NCSA Open Source License. See LICENSE.TXT for details.
+ */
+package edu.illinois.codingspectator.ui.tests.extractsuperclass;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jface.dialogs.IDialogConstants;
+
+import edu.illinois.codingspectator.ui.tests.RefactoringLog;
+import edu.illinois.codingspectator.ui.tests.RefactoringTest;
+
+/**
+ * 
+ * @author Mohsen Vakilian
+ * @author nchen
+ * 
+ */
+public class PerformedValidMultiStepExtractSuperclassTest extends RefactoringTest {
+
+	private static final String EXTRACT_SUPERCLASS_MENU_ITEM= "Extract Superclass...";
+
+	protected static final String SUPERCLASS_NAME_LABEL= "Superclass name:";
+
+	RefactoringLog refactoringLog= new RefactoringLog(RefactoringLog.LogType.CANCELLED);
+
+	@Override
+	protected String getTestFileName() {
+		return "ExtractSuperclassTestFile";
+	}
+
+	@Override
+	protected String getTestInputLocation() {
+		return "extract-superclass";
+	}
+
+	@Override
+	protected void doRefactoringLogShouldBeEmpty() {
+		assertFalse(refactoringLog.exists());
+	}
+
+	@Override
+	protected void doExecuteRefactoring() {
+		bot.selectElementToRefactor(getTestFileName(), 11, 16, "methodToBePulledUp".length());
+		bot.invokeRefactoringFromMenu(EXTRACT_SUPERCLASS_MENU_ITEM);
+		bot.fillTextField(SUPERCLASS_NAME_LABEL, getTestFileName() + "Parent");
+		bot.clickButtons(IDialogConstants.NEXT_LABEL, IDialogConstants.NEXT_LABEL, IDialogConstants.CANCEL_LABEL);
+	}
+
+	@Override
+	protected void doRefactoringShouldBeLogged() {
+		assertTrue(refactoringLog.exists());
+	}
+
+	@Override
+	protected void doCleanRefactoringHistory() throws CoreException {
+		refactoringLog.clean();
+	}
+
+}
