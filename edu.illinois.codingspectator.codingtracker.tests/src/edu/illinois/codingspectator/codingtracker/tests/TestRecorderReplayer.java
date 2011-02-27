c1da87862eccc5ab245438b05e65f7ff332b3ee6
Wanderer777 <negara2003@yahoo.com>
2011-02-26 18:14:24 -0600

    Implemented record/replay test.

--- edu.illinois.codingspectator.codingtracker.tests/src/edu/illinois/codingspectator/codingtracker/tests/TestRecorderReplayer.java
+++ edu.illinois.codingspectator.codingtracker.tests/src/edu/illinois/codingspectator/codingtracker/tests/TestRecorderReplayer.java
@@ -3,20 +3,26 @@
  */
 package edu.illinois.codingspectator.codingtracker.tests;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import java.io.File;
+import java.util.Iterator;
 import java.util.List;
 
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.junit.Test;
 
 import edu.illinois.codingspectator.codingtracker.helpers.FileHelper;
 import edu.illinois.codingspectator.codingtracker.operations.OperationDeserializer;
 import edu.illinois.codingspectator.codingtracker.operations.UserOperation;
+import edu.illinois.codingspectator.codingtracker.operations.starts.StartedEclipseOperation;
 
 /**
  * 
  * @author Stas Negara
  * 
- *         TODO: Implement this test case
  */
 public class TestRecorderReplayer extends TestCodingTracker {
 
@@ -24,35 +30,65 @@ public class TestRecorderReplayer extends TestCodingTracker {
 
 	@Test
 	public void shouldReplayAndRecord() {
-		replayAllOperations();
+		List<UserOperation> predefinedUserOperations= loadUserOperationsFromFile(operationsRecordFile);
+		replayUserOperations(predefinedUserOperations);
+		List<UserOperation> generatedUserOperations= loadUserOperationsFromFile(mainRecordFile);
+		checkEquivalencyOfUserOperations(predefinedUserOperations, generatedUserOperations);
+		checkFinalCode();
 	}
 
-	private void replayAllOperations() {
-		String operationsRecord= FileHelper.getFileContent(operationsRecordFile);
-		final List<UserOperation> userOperations= OperationDeserializer.getUserOperations(operationsRecord);
-		//FIXME: Remove @Override from Test2.java because otherwise it can not be refactored (i.e. no changes to the code).
-//		int count= 0;
-//		for (final UserOperation userOperation : userOperations) {
-//			Display.getDefault().syncExec(new Runnable() {
-//				@Override
-//				public void run() {
-//					try {
-//						userOperation.replay();
-//					} catch (Exception e) {
-//						throw new RuntimeException("Failed to replay operation: " + userOperation, e);
-//					}
-//				}
-//			});
-//			try {
-//				Thread.sleep(500);
-//			} catch (InterruptedException e) {
-//				e.printStackTrace();
-//			}
-//			count++;
-//			if (count > 116) {
-//				System.out.println("count: " + count);
-//			}
-//		}
+	private void checkEquivalencyOfUserOperations(List<UserOperation> predefinedUserOperations, List<UserOperation> generatedUserOperations) {
+		Iterator<UserOperation> generatedUserOperationsIterator= generatedUserOperations.iterator();
+		for (UserOperation predefinedUserOperation : predefinedUserOperations) {
+			//Skip Eclipse startups because they are not reproduced by the test
+			if (predefinedUserOperation instanceof StartedEclipseOperation) {
+				continue;
+			}
+			assertTrue(generatedUserOperationsIterator.hasNext());
+			UserOperation generatedUserOperation= generatedUserOperationsIterator.next();
+			assertTrue(predefinedUserOperation.getClass() == generatedUserOperation.getClass());
+			assertEquals(removeTimestamp(predefinedUserOperation), removeTimestamp(generatedUserOperation));
+		}
+		assertFalse(generatedUserOperationsIterator.hasNext()); //there should be no other generated operations
+	}
+
+	private void checkFinalCode() {
+		File predefinedTest1= new File("test-files/02/Test1.java");
+		File predefinedTest2= new File("test-files/02/Test2.java");
+		File generatedTest1= getGeneratedFile("Test1.java");
+		File generatedTest2= getGeneratedFile("Test2.java");
+		checkFilesAreEqual(predefinedTest1, generatedTest1);
+		checkFilesAreEqual(predefinedTest2, generatedTest2);
+	}
+
+	private File getGeneratedFile(String fileName) {
+		String workspaceRelativeFilePath= "/edu.illinois.testproject/src/edu/illinois/test/" + fileName;
+		return new File(ResourcesPlugin.getWorkspace().getRoot().findMember(workspaceRelativeFilePath).getLocation().toPortableString());
+	}
+
+	private void checkFilesAreEqual(File file1, File file2) {
+		assertEquals(FileHelper.getFileContent(file1), FileHelper.getFileContent(file2));
+	}
+
+	private void replayUserOperations(List<UserOperation> userOperations) {
+		for (UserOperation userOperation : userOperations) {
+			try {
+				userOperation.replay();
+			} catch (Exception e) {
+				throw new RuntimeException("Could not replay operation: " + userOperation, e);
+			}
+		}
+	}
+
+	private List<UserOperation> loadUserOperationsFromFile(File recordFile) {
+		String operationsRecord= FileHelper.getFileContent(recordFile);
+		return OperationDeserializer.getUserOperations(operationsRecord);
+	}
+
+	private String removeTimestamp(UserOperation userOperation) {
+		String userOperationString= userOperation.toString();
+		int timestampIndex= userOperationString.lastIndexOf("Timestamp: ");
+		return userOperationString.substring(0, timestampIndex);
 	}
 
 }
---
+++ edu.illinois.codingspectator.codingtracker.tests/test-files/02/Test2.java
@@ -0,0 +1,10 @@
+package edu.illinois.test;
+
+public class Test2 extends Test1 {
+
+	         
+	public void testt() {
+		System.out.println("Test2");
+	}
+	
+}
