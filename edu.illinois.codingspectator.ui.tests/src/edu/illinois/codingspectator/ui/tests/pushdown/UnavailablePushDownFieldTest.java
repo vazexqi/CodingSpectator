595fda0e387470dfc119697a20b9073b5ee2c361
Mohsen Vakilian <reprogrammer@gmail.com>
2011-03-05 15:13:03 -0600

    Verified the captured descriptor for an unavailable push down more strictly.
    This commit belongs to issue #138.

--- edu.illinois.codingspectator.ui.tests/src/edu/illinois/codingspectator/ui/tests/pushdown/UnavailablePushDownFieldTest.java
+++ edu.illinois.codingspectator.ui.tests/src/edu/illinois/codingspectator/ui/tests/pushdown/UnavailablePushDownFieldTest.java
@@ -3,12 +3,20 @@
  */
 package edu.illinois.codingspectator.ui.tests.pushdown;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
+import java.util.Collection;
+
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jdt.core.refactoring.IJavaRefactorings;
+import org.eclipse.jdt.core.refactoring.descriptors.JavaRefactoringDescriptor;
 import org.eclipse.jface.dialogs.IDialogConstants;
 
+import edu.illinois.codingspectator.ui.tests.CapturedRefactoringDescriptor;
+import edu.illinois.codingspectator.ui.tests.Encryptor.EncryptionException;
 import edu.illinois.codingspectator.ui.tests.RefactoringLog;
 import edu.illinois.codingspectator.ui.tests.RefactoringTest;
 
@@ -22,6 +30,8 @@ import edu.illinois.codingspectator.ui.tests.RefactoringTest;
 public class UnavailablePushDownFieldTest extends RefactoringTest {
 
 	private static final String PUSH_DOWN_MENU_ITEM= "Push Down...";
+
+	private static final String SELECTION= "Child2";
 
 	RefactoringLog refactoringLog= new RefactoringLog(RefactoringLog.LogType.UNAVAILABLE);
 
@@ -42,14 +52,32 @@ public class UnavailablePushDownFieldTest extends RefactoringTest {
 
 	@Override
 	protected void doExecuteRefactoring() {
-		bot.selectElementToRefactor(getTestFileFullName(), 14, 6, "Child2".length());
+		bot.selectElementToRefactor(getTestFileFullName(), 14, 6, SELECTION.length());
 		bot.invokeRefactoringFromMenu(PUSH_DOWN_MENU_ITEM);
 		bot.clickButtons(IDialogConstants.OK_LABEL);
 	}
 
 	@Override
-	protected void doRefactoringShouldBeLogged() {
+	protected void doRefactoringShouldBeLogged() throws EncryptionException {
 		assertTrue(refactoringLog.exists());
+		Collection<JavaRefactoringDescriptor> refactoringDescriptors= refactoringLog.getRefactoringDescriptors(getProjectName());
+		assertEquals(1, refactoringDescriptors.size());
+		JavaRefactoringDescriptor descriptor= refactoringDescriptors.iterator().next();
+		CapturedRefactoringDescriptor capturedDescriptor= new CapturedRefactoringDescriptor(descriptor);
+		assertTrue(capturedDescriptor.getTimestamp() > 0);
+		assertEquals("", capturedDescriptor.getComment());
+		assertEquals("CODINGSPECTATOR: RefactoringDescriptor from an unavailable refactoring", capturedDescriptor.getDescription());
+		assertEquals(0, capturedDescriptor.getFlags());
+		assertEquals(IJavaRefactorings.PUSH_DOWN, capturedDescriptor.getID());
+		assertEquals(getProjectName(), capturedDescriptor.getProject());
+		assertNull(capturedDescriptor.getElement());
+		assertNull(capturedDescriptor.getName());
+		assertFalse(capturedDescriptor.doesReference());
+//		assertEquals(SELECTION, capturedDescriptor.getSelection());
+//		assertEquals("332 0", capturedDescriptor.getSelectionOffset());
+		assertEquals("To activate this refactoring, please select the name of a non-binary instance method or field.", capturedDescriptor.getStatus());
+//		assertEquals("509e14617a2628706da3cb61b4c8cb93", Encryptor.toMD5(capturedDescriptor.getCodeSnippet()));
+		assertFalse(capturedDescriptor.isInvokedByQuickAssist());
 	}
 
 	@Override
