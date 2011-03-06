f45949d8ecab0027711077e1b1f7d6914134e7dd
Mohsen Vakilian <reprogrammer@gmail.com>
2011-03-06 10:37:08 -0600

    Improved the test for an unavailable extract method refactoring.
    Added more assertions to verify the captured descriptor for an unavailable extract method refactoring.
    
    Renamed the class for the testing unavailable extract method refactoring.
    
    This commit belongs to issue #138.

---
+++ edu.illinois.codingspectator.ui.tests/src/edu/illinois/codingspectator/ui/tests/extractmethod/UnavailableExtractMethodTest.java
@@ -0,0 +1,77 @@
+package edu.illinois.codingspectator.ui.tests.extractmethod;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Collection;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jdt.core.refactoring.IJavaRefactorings;
+import org.eclipse.jdt.core.refactoring.descriptors.JavaRefactoringDescriptor;
+import org.eclipse.jface.dialogs.IDialogConstants;
+
+import edu.illinois.codingspectator.ui.tests.CapturedRefactoringDescriptor;
+import edu.illinois.codingspectator.ui.tests.RefactoringLog;
+import edu.illinois.codingspectator.ui.tests.RefactoringTest;
+
+public class UnavailableExtractMethodTest extends RefactoringTest {
+
+	protected static final String EXTRACT_METHOD_MENU_ITEM_NAME= "Extract Method...";
+
+	private static final String SELECTION= "main";
+
+	RefactoringLog refactoringLog= new RefactoringLog(RefactoringLog.LogType.UNAVAILABLE);
+
+	@Override
+	protected String getTestFileName() {
+		return "ValidExtractMethodTestFile";
+	}
+
+	@Override
+	protected String getTestInputLocation() {
+		return "extract-method";
+	}
+
+	@Override
+	protected void doRefactoringLogShouldBeEmpty() {
+		assertFalse(refactoringLog.exists());
+	}
+
+	@Override
+	protected void doExecuteRefactoring() {
+		bot.selectElementToRefactor(getTestFileFullName(), 9, 23, SELECTION.length());
+		bot.invokeRefactoringFromMenu(EXTRACT_METHOD_MENU_ITEM_NAME);
+		bot.clickButtons(IDialogConstants.OK_LABEL);
+	}
+
+	@Override
+	protected void doRefactoringShouldBeLogged() {
+		assertTrue(refactoringLog.exists());
+		Collection<JavaRefactoringDescriptor> refactoringDescriptors= refactoringLog.getRefactoringDescriptors(getProjectName());
+		assertEquals(1, refactoringDescriptors.size());
+		JavaRefactoringDescriptor descriptor= refactoringDescriptors.iterator().next();
+		CapturedRefactoringDescriptor capturedDescriptor= new CapturedRefactoringDescriptor(descriptor);
+		assertTrue(capturedDescriptor.getTimestamp() > 0);
+		assertEquals("", capturedDescriptor.getComment());
+		assertEquals("CODINGSPECTATOR: RefactoringDescriptor from an unavailable refactoring", capturedDescriptor.getDescription());
+		assertEquals(0, capturedDescriptor.getFlags());
+		assertEquals(IJavaRefactorings.EXTRACT_METHOD, capturedDescriptor.getID());
+		assertEquals(getProjectName(), capturedDescriptor.getProject());
+		assertNull(capturedDescriptor.getElement());
+		assertNull(capturedDescriptor.getName());
+		assertFalse(capturedDescriptor.doesReference());
+		assertEquals(SELECTION, capturedDescriptor.getSelection());
+//		assertEquals("332 0", capturedDescriptor.getSelectionOffset());
+		assertEquals("Cannot extract new method from selection. Only statements from a method body can be extracted.", capturedDescriptor.getStatus());
+//		assertEquals("509e14617a2628706da3cb61b4c8cb93", Encryptor.toMD5(capturedDescriptor.getCodeSnippet()));
+		assertFalse(capturedDescriptor.isInvokedByQuickAssist());
+	}
+
+	@Override
+	protected void doCleanRefactoringHistory() throws CoreException {
+		refactoringLog.clean();
+	}
+
+}
