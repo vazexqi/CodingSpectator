8c5d4659c8df75ce1dc21020995bcb6f8fd85dd5
Balaji Ambresh Rajkumar <rockmylife@gmail.com>
2011-03-12 12:04:43 -0500

    Added test cases for moving a JAVA compilation unit to a folder.
    This commit belongs to issue #124.

---
+++ edu.illinois.codingspectator.ui.tests/src/edu/illinois/codingspectator/ui/tests/move/ValidCancelledMoveFileToContainerTest.java
@@ -0,0 +1,57 @@
+/**
+ * This file is licensed under the University of Illinois/NCSA Open Source License. See LICENSE.TXT for details.
+ */
+package edu.illinois.codingspectator.ui.tests.move;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jface.dialogs.IDialogConstants;
+
+import edu.illinois.codingspectator.ui.tests.RefactoringLog;
+import edu.illinois.codingspectator.ui.tests.RefactoringTest;
+
+/**
+ * @author Balaji Ambresh Rajkumar
+ */
+public class ValidCancelledMoveFileToContainerTest extends RefactoringTest {
+
+	RefactoringLog refactoringLog= new RefactoringLog(RefactoringLog.LogType.CANCELLED);
+
+	@Override
+	protected String getTestFileName() {
+		return "MoveCusTestFile";
+	}
+
+	@Override
+	protected String getTestInputLocation() {
+		return "move";
+	}
+
+	@Override
+	protected void doRefactoringLogShouldBeEmpty() {
+		assertFalse(refactoringLog.exists());
+	}
+
+	@Override
+	protected void doExecuteRefactoring() {
+		bot.selectFromPackageExplorer(getProjectName(), "src", "edu.illinois.codingspectator", getTestFileFullName());
+		bot.invokeRefactoringFromMenu("Move...");
+		bot.activateShellWithName("Move");
+		bot.getCurrentTree().pressShortcut(org.eclipse.jface.bindings.keys.KeyStroke.getInstance('.'));
+		
+		bot.clickButtons(IDialogConstants.CANCEL_LABEL);
+	}
+
+	@Override
+	protected void doRefactoringShouldBeLogged() {
+		assertTrue(refactoringLog.exists());
+	}
+
+	@Override
+	protected void doCleanRefactoringHistory() throws CoreException {
+		refactoringLog.clean();
+	}
+
+}
