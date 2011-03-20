894c0c6562969b14184b56e24d9442b92574ff89
Mohsen Vakilian <reprogrammer@gmail.com>
2011-03-20 09:33:09 -0500

    Removed duplication in UnavailableExtractConstantTest.
    Made UnavailableExtractConstantTest reuse the infrastructure for verifying descriptors.
    
    This commit belongs to issue #138.

--- edu.illinois.codingspectator.ui.tests/src/edu/illinois/codingspectator/ui/tests/extractconstant/UnavailableExtractConstantTest.java
+++ edu.illinois.codingspectator.ui.tests/src/edu/illinois/codingspectator/ui/tests/extractconstant/UnavailableExtractConstantTest.java
@@ -1,14 +1,12 @@
 package edu.illinois.codingspectator.ui.tests.extractconstant;
 
-import static org.junit.Assert.assertFalse;
+import java.util.Arrays;
+import java.util.Collection;
 
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.dialogs.IDialogConstants;
 
-import edu.illinois.codingspectator.ui.tests.CapturedRefactoringDescriptor;
-import edu.illinois.codingspectator.ui.tests.DescriptorComparator;
-import edu.illinois.codingspectator.ui.tests.RefactoringLog;
-import edu.illinois.codingspectator.ui.tests.RefactoringLogUtils;
+import edu.illinois.codingspectator.ui.tests.RefactoringLog.LogType;
+import edu.illinois.codingspectator.ui.tests.RefactoringLogChecker;
 import edu.illinois.codingspectator.ui.tests.RefactoringTest;
 
 public class UnavailableExtractConstantTest extends RefactoringTest {
@@ -18,8 +16,6 @@ public class UnavailableExtractConstantTest extends RefactoringTest {
 	private static final String TEST_FILE_NAME= "ExtractConstantTestFile";
 
 	private final String SELECTION= "main";
-
-	RefactoringLog refactoringLog= new RefactoringLog(RefactoringLog.LogType.UNAVAILABLE);
 
 	@Override
 	protected String getTestFileName() {
@@ -32,8 +28,8 @@ public class UnavailableExtractConstantTest extends RefactoringTest {
 	}
 
 	@Override
-	protected void doRefactoringLogShouldBeEmpty() {
-		assertFalse(refactoringLog.exists());
+	protected Collection<RefactoringLogChecker> getRefactoringLogCheckers() {
+		return Arrays.asList(new RefactoringLogChecker(LogType.UNAVAILABLE, getTestInputLocation(), getClass().getSimpleName(), getProjectName()));
 	}
 
 	@Override
@@ -42,19 +38,5 @@ public class UnavailableExtractConstantTest extends RefactoringTest {
 		bot.invokeRefactoringFromMenu(EXTRACT_CONSTANT_MENU_ITEM);
 		bot.clickButtons(IDialogConstants.OK_LABEL);
 	}
-
-	@Override
-	protected void doRefactoringShouldBeLogged() {
-		CapturedRefactoringDescriptor capturedDescriptor= RefactoringLogUtils.getTheSingleRefactoringDescriptor(refactoringLog, getProjectName());
-		CapturedRefactoringDescriptor expectedRefactoringDescriptor= RefactoringLogUtils.getTheSingleExpectedRefactoringDescriptor(getTestInputLocation() + "/" + getClass().getSimpleName(),
-				getProjectName());
-		DescriptorComparator.assertMatches(expectedRefactoringDescriptor, capturedDescriptor);
-	}
-
-	@Override
-	protected void doCleanRefactoringHistory() throws CoreException {
-		refactoringLog.clean();
-	}
-
 
 }
