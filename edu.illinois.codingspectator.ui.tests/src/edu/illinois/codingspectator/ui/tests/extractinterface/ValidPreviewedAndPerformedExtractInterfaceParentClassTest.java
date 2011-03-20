e01a8ccb4620732cc2e236492979fb87eb3cbee1
Mohsen Vakilian <reprogrammer@gmail.com>
2011-03-19 19:21:54 -0500

    Refactored ValidPreviewedAndPerformedExtractInterfaceParentClassTest.
    Refactored ValidPreviewedAndPerformedExtractInterfaceParentClassTest to use the file-based technique for checking the correctness of the captured descriptors.
    
    This commit belongs to issue #138.

--- edu.illinois.codingspectator.ui.tests/src/edu/illinois/codingspectator/ui/tests/extractinterface/ValidPreviewedAndPerformedExtractInterfaceParentClassTest.java
+++ edu.illinois.codingspectator.ui.tests/src/edu/illinois/codingspectator/ui/tests/extractinterface/ValidPreviewedAndPerformedExtractInterfaceParentClassTest.java
@@ -1,29 +1,14 @@
 package edu.illinois.codingspectator.ui.tests.extractinterface;
 
-import static org.hamcrest.text.pattern.Patterns.anyCharacterInCategory;
-import static org.hamcrest.text.pattern.Patterns.oneOrMore;
-import static org.hamcrest.text.pattern.Patterns.sequence;
-import static org.hamcrest.text.pattern.Patterns.text;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
-
-import java.util.Collection;
 
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.jdt.core.refactoring.IJavaRefactorings;
-import org.eclipse.jdt.core.refactoring.descriptors.JavaRefactoringDescriptor;
 import org.eclipse.jface.dialogs.IDialogConstants;
-import org.hamcrest.text.pattern.PatternComponent;
-import org.hamcrest.text.pattern.PatternMatcher;
 
 import edu.illinois.codingspectator.ui.tests.CapturedRefactoringDescriptor;
-import edu.illinois.codingspectator.ui.tests.CodingSpectatorBot;
-import edu.illinois.codingspectator.ui.tests.Encryptor;
-import edu.illinois.codingspectator.ui.tests.Encryptor.EncryptionException;
+import edu.illinois.codingspectator.ui.tests.DescriptorComparator;
 import edu.illinois.codingspectator.ui.tests.RefactoringLog;
+import edu.illinois.codingspectator.ui.tests.RefactoringLogUtils;
 import edu.illinois.codingspectator.ui.tests.RefactoringTest;
 
 public class ValidPreviewedAndPerformedExtractInterfaceParentClassTest extends RefactoringTest {
@@ -37,14 +22,6 @@ public class ValidPreviewedAndPerformedExtractInterfaceParentClassTest extends R
 	RefactoringLog performedRefactoringLog= new RefactoringLog(RefactoringLog.LogType.PERFORMED);
 
 	RefactoringLog eclipseRefactoringLog= new RefactoringLog(RefactoringLog.LogType.ECLIPSE);
-
-	private String getSelectedClassFullQualifiedName() {
-		return CodingSpectatorBot.PACKAGE_NAME + "." + SELECTION;
-	}
-
-	private String getNewInterfaceFullQualifiedName() {
-		return CodingSpectatorBot.PACKAGE_NAME + "." + NEW_INTERFACE_NAME;
-	}
 
 	@Override
 	protected String getTestFileName() {
@@ -72,71 +49,23 @@ public class ValidPreviewedAndPerformedExtractInterfaceParentClassTest extends R
 	}
 
 	@Override
-	protected void doRefactoringShouldBeLogged() throws EncryptionException {
+	protected void doRefactoringShouldBeLogged() {
 		performedLogShouldBeCorrect();
 		eclipseLogShouldBeCorrect();
 	}
 
-	private void performedLogShouldBeCorrect() throws EncryptionException {
-		assertTrue(performedRefactoringLog.exists());
-		Collection<JavaRefactoringDescriptor> refactoringDescriptors= performedRefactoringLog.getRefactoringDescriptors(getProjectName());
-		assertEquals(1, refactoringDescriptors.size());
-		JavaRefactoringDescriptor descriptor= refactoringDescriptors.iterator().next();
-		CapturedRefactoringDescriptor capturedDescriptor= new CapturedRefactoringDescriptor(descriptor);
-		capturedRefactoringDescriptorShouldBeCorrect(capturedDescriptor);
-		codingspectatorAttributesShouldBeCorrect(capturedDescriptor);
+	private void performedLogShouldBeCorrect() {
+		CapturedRefactoringDescriptor capturedDescriptor= RefactoringLogUtils.getTheSingleRefactoringDescriptor(performedRefactoringLog, getProjectName());
+		CapturedRefactoringDescriptor expectedRefactoringDescriptor= RefactoringLogUtils.getTheSingleExpectedRefactoringDescriptor(getTestInputLocation() + "/" + getClass().getSimpleName()
+				+ "/performed", getProjectName());
+		DescriptorComparator.assertMatches(expectedRefactoringDescriptor, capturedDescriptor);
 	}
 
-	private void eclipseLogShouldBeCorrect() throws EncryptionException {
-		assertTrue(eclipseRefactoringLog.exists());
-		Collection<JavaRefactoringDescriptor> refactoringDescriptors= eclipseRefactoringLog.getRefactoringDescriptors(getProjectName());
-		assertEquals(1, refactoringDescriptors.size());
-		JavaRefactoringDescriptor descriptor= refactoringDescriptors.iterator().next();
-		CapturedRefactoringDescriptor capturedDescriptor= new CapturedRefactoringDescriptor(descriptor);
-		capturedRefactoringDescriptorShouldBeCorrect(capturedDescriptor);
-	}
-
-	private void codingspectatorAttributesShouldBeCorrect(CapturedRefactoringDescriptor capturedDescriptor) throws EncryptionException {
-		assertEquals(SELECTION, capturedDescriptor.getSelectionText());
-		assertNull(capturedDescriptor.getSelectionInCodeSnippet());
-		assertEquals("<OK\n>", capturedDescriptor.getStatus());
-		assertEquals("44ee7d45a160da76d50b585a3d25a038", Encryptor.toMD5(capturedDescriptor.getCodeSnippet()));
-		assertFalse(capturedDescriptor.isInvokedByQuickAssist());
-		PatternComponent timestampPattern= oneOrMore(anyCharacterInCategory("Digit"));
-		PatternMatcher expectedNavigationHistoryPatternMatcher= new PatternMatcher(sequence(text("{[Extract Interface,BEGIN_REFACTORING,"), timestampPattern,
-				text("],[PreviewPage,Previe&w >,"), timestampPattern, text("],[PreviewPage,OK,"), timestampPattern, text("],}")));
-		assertThat(capturedDescriptor.getNavigationHistory(), expectedNavigationHistoryPatternMatcher);
-	}
-
-	private void capturedRefactoringDescriptorShouldBeCorrect(CapturedRefactoringDescriptor capturedDescriptor) throws EncryptionException {
-		javaAttributesShouldBeCorrect(capturedDescriptor);
-		attributesSpecificToExtractInterfaceShouldBeCorrect(capturedDescriptor);
-	}
-
-	private void javaAttributesShouldBeCorrect(CapturedRefactoringDescriptor capturedDescriptor) {
-		assertTrue(capturedDescriptor.getTimestamp() > 0);
-		assertEquals(String.format("Extract interface '%s' from '%s'\n" +
-				"- Original project: '%s'\n" +
-				"- Original element: '%s'\n" +
-				"- Extracted interface: '%s'\n" +
-				"- Use super type where possible", getNewInterfaceFullQualifiedName(), getSelectedClassFullQualifiedName(), getProjectName(), getSelectedClassFullQualifiedName(),
-				getNewInterfaceFullQualifiedName()),
-				capturedDescriptor.getComment());
-		assertEquals(String.format("/src<%s{%s[%s", CodingSpectatorBot.PACKAGE_NAME, getTestFileFullName(), SELECTION), capturedDescriptor.getInput());
-		assertEquals(String.format("Extract interface '%s'", NEW_INTERFACE_NAME), capturedDescriptor.getDescription());
-		assertEquals(589830, capturedDescriptor.getFlags());
-		assertEquals(IJavaRefactorings.EXTRACT_INTERFACE, capturedDescriptor.getID());
-		assertEquals(getProjectName(), capturedDescriptor.getProject());
-		assertNull(capturedDescriptor.getElement());
-		assertNull(capturedDescriptor.getSelection());
-	}
-
-	private void attributesSpecificToExtractInterfaceShouldBeCorrect(CapturedRefactoringDescriptor capturedDescriptor) {
-		assertTrue(capturedDescriptor.getAbstract());
-		assertTrue(capturedDescriptor.getComments());
-		assertFalse(capturedDescriptor.getInstanceOf());
-		assertTrue(capturedDescriptor.getPublic());
-		assertEquals(NEW_INTERFACE_NAME, capturedDescriptor.getName());
+	private void eclipseLogShouldBeCorrect() {
+		CapturedRefactoringDescriptor capturedDescriptor= RefactoringLogUtils.getTheSingleRefactoringDescriptor(eclipseRefactoringLog, getProjectName());
+		CapturedRefactoringDescriptor expectedRefactoringDescriptor= RefactoringLogUtils.getTheSingleExpectedRefactoringDescriptor(getTestInputLocation() + "/" + getClass().getSimpleName()
+				+ "/eclipse", getProjectName());
+		DescriptorComparator.assertMatches(expectedRefactoringDescriptor, capturedDescriptor);
 	}
 
 	@Override
