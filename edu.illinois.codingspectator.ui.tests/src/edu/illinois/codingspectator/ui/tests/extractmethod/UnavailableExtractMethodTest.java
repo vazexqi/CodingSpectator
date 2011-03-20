bebf29c0df3bcb2152664b46caf6cc8e1c25659e
Mohsen Vakilian <reprogrammer@gmail.com>
2011-03-19 19:49:22 -0500

    Refactored UnavailableExtractMethodTest.
    Refactored UnavailableExtractMethodTest to the file comparison based style of verifying the captured refactoring descriptors.
    
    This commit belongs to issue #138.

--- edu.illinois.codingspectator.ui.tests/src/edu/illinois/codingspectator/ui/tests/extractmethod/UnavailableExtractMethodTest.java
+++ edu.illinois.codingspectator.ui.tests/src/edu/illinois/codingspectator/ui/tests/extractmethod/UnavailableExtractMethodTest.java
@@ -1,21 +1,14 @@
 package edu.illinois.codingspectator.ui.tests.extractmethod;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
-
-import java.util.Collection;
 
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.jdt.core.refactoring.IJavaRefactorings;
-import org.eclipse.jdt.core.refactoring.descriptors.JavaRefactoringDescriptor;
 import org.eclipse.jface.dialogs.IDialogConstants;
 
 import edu.illinois.codingspectator.ui.tests.CapturedRefactoringDescriptor;
-import edu.illinois.codingspectator.ui.tests.Encryptor;
-import edu.illinois.codingspectator.ui.tests.Encryptor.EncryptionException;
+import edu.illinois.codingspectator.ui.tests.DescriptorComparator;
 import edu.illinois.codingspectator.ui.tests.RefactoringLog;
+import edu.illinois.codingspectator.ui.tests.RefactoringLogUtils;
 import edu.illinois.codingspectator.ui.tests.RefactoringTest;
 
 public class UnavailableExtractMethodTest extends RefactoringTest {
@@ -49,26 +42,11 @@ public class UnavailableExtractMethodTest extends RefactoringTest {
 	}
 
 	@Override
-	protected void doRefactoringShouldBeLogged() throws EncryptionException {
-		assertTrue(refactoringLog.exists());
-		Collection<JavaRefactoringDescriptor> refactoringDescriptors= refactoringLog.getRefactoringDescriptors(getProjectName());
-		assertEquals(1, refactoringDescriptors.size());
-		JavaRefactoringDescriptor descriptor= refactoringDescriptors.iterator().next();
-		CapturedRefactoringDescriptor capturedDescriptor= new CapturedRefactoringDescriptor(descriptor);
-		assertTrue(capturedDescriptor.getTimestamp() > 0);
-		assertEquals("", capturedDescriptor.getComment());
-		assertEquals("CODINGSPECTATOR: RefactoringDescriptor from an unavailable refactoring", capturedDescriptor.getDescription());
-		assertEquals(0, capturedDescriptor.getFlags());
-		assertEquals(IJavaRefactorings.EXTRACT_METHOD, capturedDescriptor.getID());
-		assertEquals(getProjectName(), capturedDescriptor.getProject());
-		assertNull(capturedDescriptor.getElement());
-		assertNull(capturedDescriptor.getName());
-		assertFalse(capturedDescriptor.doesReference());
-		assertEquals(SELECTION, capturedDescriptor.getSelectionText());
-		assertEquals("277 4", capturedDescriptor.getSelectionInCodeSnippet());
-		assertEquals("Cannot extract new method from selection. Only statements from a method body can be extracted.", capturedDescriptor.getStatus());
-		assertEquals("4b7e3084104f0f3fe5be59d5b8618565", Encryptor.toMD5(capturedDescriptor.getCodeSnippet()));
-		assertFalse(capturedDescriptor.isInvokedByQuickAssist());
+	protected void doRefactoringShouldBeLogged() {
+		CapturedRefactoringDescriptor capturedDescriptor= RefactoringLogUtils.getTheSingleRefactoringDescriptor(refactoringLog, getProjectName());
+		CapturedRefactoringDescriptor expectedRefactoringDescriptor= RefactoringLogUtils.getTheSingleExpectedRefactoringDescriptor(getTestInputLocation() + "/" + getClass().getSimpleName(),
+				getProjectName());
+		DescriptorComparator.assertMatches(expectedRefactoringDescriptor, capturedDescriptor);
 	}
 
 	@Override
