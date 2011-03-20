8b9a223596d94b8e6ddf8741a98c4c7cb8aef0b1
Mohsen Vakilian <reprogrammer@gmail.com>
2011-03-20 09:28:59 -0500

    Removed duplication in PerformedExtractConstantTest.
    Made PerformedExtractConstantTest reuse the infrastructure for verifying descriptors.
    
    This commit belongs to issue #138.

--- edu.illinois.codingspectator.ui.tests/src/edu/illinois/codingspectator/ui/tests/extractconstant/PerformedExtractConstantTest.java
+++ edu.illinois.codingspectator.ui.tests/src/edu/illinois/codingspectator/ui/tests/extractconstant/PerformedExtractConstantTest.java
@@ -3,15 +3,13 @@
  */
 package edu.illinois.codingspectator.ui.tests.extractconstant;
 
-import static org.junit.Assert.assertFalse;
+import java.util.Arrays;
+import java.util.Collection;
 
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.dialogs.IDialogConstants;
 
-import edu.illinois.codingspectator.ui.tests.CapturedRefactoringDescriptor;
-import edu.illinois.codingspectator.ui.tests.DescriptorComparator;
-import edu.illinois.codingspectator.ui.tests.RefactoringLog;
-import edu.illinois.codingspectator.ui.tests.RefactoringLogUtils;
+import edu.illinois.codingspectator.ui.tests.RefactoringLog.LogType;
+import edu.illinois.codingspectator.ui.tests.RefactoringLogChecker;
 import edu.illinois.codingspectator.ui.tests.RefactoringTest;
 
 /**
@@ -26,10 +24,6 @@ public class PerformedExtractConstantTest extends RefactoringTest {
 
 	private static final String SELECTION= "\"Test0\"";
 
-	RefactoringLog performedRefactoringLog= new RefactoringLog(RefactoringLog.LogType.PERFORMED);
-
-	RefactoringLog eclipseRefactoringLog= new RefactoringLog(RefactoringLog.LogType.ECLIPSE);
-
 	@Override
 	protected String getTestFileName() {
 		return TEST_FILE_NAME;
@@ -41,8 +35,9 @@ public class PerformedExtractConstantTest extends RefactoringTest {
 	}
 
 	@Override
-	protected void doRefactoringLogShouldBeEmpty() {
-		assertFalse(eclipseRefactoringLog.exists());
+	protected Collection<RefactoringLogChecker> getRefactoringLogCheckers() {
+		return Arrays.asList(new RefactoringLogChecker(LogType.PERFORMED, getTestInputLocation(), getClass().getSimpleName(), getProjectName()), new RefactoringLogChecker(LogType.ECLIPSE,
+				getTestInputLocation(), getClass().getSimpleName(), getProjectName()));
 	}
 
 	@Override
@@ -50,32 +45,6 @@ public class PerformedExtractConstantTest extends RefactoringTest {
 		bot.selectElementToRefactor(getTestFileFullName(), 8, 27, SELECTION.length());
 		bot.invokeRefactoringFromMenu(EXTRACT_CONSTANT_MENU_ITEM);
 		bot.clickButtons(IDialogConstants.OK_LABEL);
-	}
-
-	@Override
-	protected void doRefactoringShouldBeLogged() {
-		performedLogShouldBeCorrect();
-		eclipseLogShouldBeCorrect();
-	}
-
-	private void performedLogShouldBeCorrect() {
-		CapturedRefactoringDescriptor capturedDescriptor= RefactoringLogUtils.getTheSingleRefactoringDescriptor(performedRefactoringLog, getProjectName());
-		CapturedRefactoringDescriptor expectedRefactoringDescriptor= RefactoringLogUtils.getTheSingleExpectedRefactoringDescriptor(getTestInputLocation() + "/" + getClass().getSimpleName()
-				+ "/performed", getProjectName());
-		DescriptorComparator.assertMatches(expectedRefactoringDescriptor, capturedDescriptor);
-	}
-
-	private void eclipseLogShouldBeCorrect() {
-		CapturedRefactoringDescriptor capturedDescriptor= RefactoringLogUtils.getTheSingleRefactoringDescriptor(eclipseRefactoringLog, getProjectName());
-		CapturedRefactoringDescriptor expectedRefactoringDescriptor= RefactoringLogUtils.getTheSingleExpectedRefactoringDescriptor(getTestInputLocation() + "/" + getClass().getSimpleName()
-				+ "/eclipse", getProjectName());
-		DescriptorComparator.assertMatches(expectedRefactoringDescriptor, capturedDescriptor);
-	}
-
-	@Override
-	protected void doCleanRefactoringHistory() throws CoreException {
-		performedRefactoringLog.clean();
-		eclipseRefactoringLog.clean();
 	}
 
 }
