8f8035a708ab8127c0d652915266ee4beaa47533
Mohsen Vakilian <reprogrammer@gmail.com>
2011-03-20 08:14:14 -0500

    Refactored UnavailableInlineTest.
    Refactored UnavailableInlineTest for comparison of descriptor files.
    
    This commit belongs to issue #138.

--- edu.illinois.codingspectator.ui.tests/src/edu/illinois/codingspectator/ui/tests/inline/UnavailableInlineTest.java
+++ edu.illinois.codingspectator.ui.tests/src/edu/illinois/codingspectator/ui/tests/inline/UnavailableInlineTest.java
@@ -3,22 +3,15 @@
  */
 package edu.illinois.codingspectator.ui.tests.inline;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
-
-import java.util.Collection;
 
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.jdt.core.refactoring.IJavaRefactorings;
-import org.eclipse.jdt.core.refactoring.descriptors.JavaRefactoringDescriptor;
 import org.eclipse.jface.dialogs.IDialogConstants;
 
 import edu.illinois.codingspectator.ui.tests.CapturedRefactoringDescriptor;
-import edu.illinois.codingspectator.ui.tests.Encryptor;
-import edu.illinois.codingspectator.ui.tests.Encryptor.EncryptionException;
+import edu.illinois.codingspectator.ui.tests.DescriptorComparator;
 import edu.illinois.codingspectator.ui.tests.RefactoringLog;
+import edu.illinois.codingspectator.ui.tests.RefactoringLogUtils;
 import edu.illinois.codingspectator.ui.tests.RefactoringTest;
 
 /**
@@ -56,26 +49,11 @@ public class UnavailableInlineTest extends RefactoringTest {
 	}
 
 	@Override
-	protected void doRefactoringShouldBeLogged() throws EncryptionException {
-		assertTrue(refactoringLog.exists());
-		Collection<JavaRefactoringDescriptor> refactoringDescriptors= refactoringLog.getRefactoringDescriptors(getProjectName());
-		assertEquals(1, refactoringDescriptors.size());
-		JavaRefactoringDescriptor descriptor= refactoringDescriptors.iterator().next();
-		CapturedRefactoringDescriptor capturedDescriptor= new CapturedRefactoringDescriptor(descriptor);
-		assertTrue(capturedDescriptor.getTimestamp() > 0);
-		assertEquals("", capturedDescriptor.getComment());
-		assertEquals("CODINGSPECTATOR: RefactoringDescriptor from an unavailable refactoring", capturedDescriptor.getDescription());
-		assertEquals(0, capturedDescriptor.getFlags());
-		assertEquals(IJavaRefactorings.INLINE, capturedDescriptor.getID());
-		assertEquals(getProjectName(), capturedDescriptor.getProject());
-		assertNull(capturedDescriptor.getElement());
-		assertNull(capturedDescriptor.getName());
-		assertFalse(capturedDescriptor.doesReference());
-		assertEquals(SELECTION, capturedDescriptor.getSelectionText());
-		assertEquals(String.format("173 %d", SELECTION.length()), capturedDescriptor.getSelectionInCodeSnippet());
-		assertEquals("Select a method declaration, a method invocation, a static final field or a local variable that you want to inline.", capturedDescriptor.getStatus());
-		assertEquals("8532f1d6de06a9519645fde01f082b3b", Encryptor.toMD5(capturedDescriptor.getCodeSnippet()));
-		assertFalse(capturedDescriptor.isInvokedByQuickAssist());
+	protected void doRefactoringShouldBeLogged() {
+		CapturedRefactoringDescriptor capturedDescriptor= RefactoringLogUtils.getTheSingleRefactoringDescriptor(refactoringLog, getProjectName());
+		CapturedRefactoringDescriptor expectedRefactoringDescriptor= RefactoringLogUtils.getTheSingleExpectedRefactoringDescriptor(getTestInputLocation() + "/" + getClass().getSimpleName(),
+					getProjectName());
+		DescriptorComparator.assertMatches(expectedRefactoringDescriptor, capturedDescriptor);
 	}
 
 	@Override
