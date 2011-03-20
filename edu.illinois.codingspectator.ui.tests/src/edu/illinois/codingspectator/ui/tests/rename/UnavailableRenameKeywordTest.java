0230bd8f0dd1bb0df9323eb54813235de6e84080
Mohsen Vakilian <reprogrammer@gmail.com>
2011-03-20 08:37:08 -0500

    Refactored UnavailableRenameKeywordTest.
    Refactored UnavailableRenameKeywordTest for comparison of descriptor files.
    
    This commit belongs to issue #138.

--- edu.illinois.codingspectator.ui.tests/src/edu/illinois/codingspectator/ui/tests/rename/UnavailableRenameKeywordTest.java
+++ edu.illinois.codingspectator.ui.tests/src/edu/illinois/codingspectator/ui/tests/rename/UnavailableRenameKeywordTest.java
@@ -3,22 +3,15 @@
  */
 package edu.illinois.codingspectator.ui.tests.rename;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
-
-import java.util.Collection;
 
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.jdt.core.refactoring.IJavaRefactorings;
-import org.eclipse.jdt.core.refactoring.descriptors.JavaRefactoringDescriptor;
 import org.eclipse.jface.dialogs.IDialogConstants;
 
 import edu.illinois.codingspectator.ui.tests.CapturedRefactoringDescriptor;
-import edu.illinois.codingspectator.ui.tests.Encryptor;
-import edu.illinois.codingspectator.ui.tests.Encryptor.EncryptionException;
+import edu.illinois.codingspectator.ui.tests.DescriptorComparator;
 import edu.illinois.codingspectator.ui.tests.RefactoringLog;
+import edu.illinois.codingspectator.ui.tests.RefactoringLogUtils;
 import edu.illinois.codingspectator.ui.tests.RefactoringTest;
 
 /**
@@ -60,27 +53,11 @@ public class UnavailableRenameKeywordTest extends RefactoringTest {
 	}
 
 	@Override
-	protected void doRefactoringShouldBeLogged() throws EncryptionException {
-		assertTrue(refactoringLog.exists());
-		Collection<JavaRefactoringDescriptor> refactoringDescriptors= refactoringLog.getRefactoringDescriptors(getProjectName());
-		assertEquals(1, refactoringDescriptors.size());
-		JavaRefactoringDescriptor descriptor= refactoringDescriptors.iterator().next();
-		CapturedRefactoringDescriptor capturedDescriptor= new CapturedRefactoringDescriptor(descriptor);
-		assertTrue(capturedDescriptor.getTimestamp() > 0);
-		assertEquals("", capturedDescriptor.getComment());
-		assertEquals("CODINGSPECTATOR: RefactoringDescriptor from an unavailable refactoring", capturedDescriptor.getDescription());
-		assertEquals(0, capturedDescriptor.getFlags());
-		assertEquals(IJavaRefactorings.RENAME_UNKNOWN_JAVA_ELEMENT, capturedDescriptor.getID());
-		assertEquals(getProjectName(), capturedDescriptor.getProject());
-		assertNull(capturedDescriptor.getElement());
-		assertNull(capturedDescriptor.getName());
-		assertFalse(capturedDescriptor.doesReference());
-		assertEquals(SELECTION, capturedDescriptor.getSelectionText());
-		assertEquals(String.format("167 %d", SELECTION.length()), capturedDescriptor.getSelectionInCodeSnippet());
-		assertEquals("Operation unavailable on the current selection.\n" +
-				"Select a Java project, source folder, resource, package, compilation unit, type, field, method, parameter or a local variable", capturedDescriptor.getStatus());
-		assertEquals("fb11f9579c34707811fee7300323c9b9", Encryptor.toMD5(capturedDescriptor.getCodeSnippet()));
-		assertFalse(capturedDescriptor.isInvokedByQuickAssist());
+	protected void doRefactoringShouldBeLogged() {
+		CapturedRefactoringDescriptor capturedDescriptor= RefactoringLogUtils.getTheSingleRefactoringDescriptor(refactoringLog, getProjectName());
+		CapturedRefactoringDescriptor expectedRefactoringDescriptor= RefactoringLogUtils.getTheSingleExpectedRefactoringDescriptor(getTestInputLocation() + "/" + getClass().getSimpleName(),
+				getProjectName());
+		DescriptorComparator.assertMatches(expectedRefactoringDescriptor, capturedDescriptor);
 	}
 
 	@Override
