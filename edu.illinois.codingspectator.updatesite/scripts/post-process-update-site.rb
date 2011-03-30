1a345d457514cce242d59bdb1efe9e11c8df7a9e
Mohsen Vakilian <reprogrammer@gmail.com>
2011-03-30 12:15:24 -0500

    Cleaned up post-process-update-site.rb.
    Removed output commands from the Ruby script.
    
    This commit belongs to issue #188.

--- edu.illinois.codingspectator.updatesite/scripts/post-process-update-site.rb
+++ edu.illinois.codingspectator.updatesite/scripts/post-process-update-site.rb
@@ -2,9 +2,7 @@
 #This file is licensed under the University of Illinois/NCSA Open Source License. See LICENSE.TXT for details.
 
 $THIS_FOLDER_PATH = File.expand_path(File.dirname(__FILE__))
-puts $THIS_FOLDER_PATH
 $PARENT_OF_THIS_FOLDER_PATH = File.dirname($THIS_FOLDER_PATH)
-puts $PARENT_OF_THIS_FOLDER_PATH
 system("#{$THIS_FOLDER_PATH}/patch/widen-patch-ranges.rb #{$PARENT_OF_THIS_FOLDER_PATH} #{$THIS_FOLDER_PATH}/patch/widen-patch-ranges.xsl")
 system("#{$THIS_FOLDER_PATH}/sign/sign.rb #{$THIS_FOLDER_PATH}/sign/CodingSpectatorKeyStore")
 
