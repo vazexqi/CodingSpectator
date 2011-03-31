dbee4aca2a454879678e8a454949b71620c30f3c
Balaji Ambresh Rajkumar <rockmylife@gmail.com>
2011-03-30 23:49:15 -0400

    Added tests for verifying performed and cancelled MoveImportDeclarationsPolicy.
    This commit belongs to issue #124.

---
+++ edu.illinois.codingspectator.ui.tests/src/edu/illinois/codingspectator/ui/tests/move/T25.java
@@ -0,0 +1,40 @@
+/**
+ * This file is licensed under the University of Illinois/NCSA Open Source License. See LICENSE.TXT for details.
+ */
+package edu.illinois.codingspectator.ui.tests.move;
+
+import org.eclipse.jface.dialogs.IDialogConstants;
+import edu.illinois.codingspectator.ui.tests.CodingSpectatorBot;
+import edu.illinois.codingspectator.ui.tests.RefactoringTest;
+
+/**
+ * This tests exercises the MoveImportDeclarationsPolicy processor. It cancels the refactoring to
+ * move multiple import statements from one source file to another.
+ * 
+ * @author Balaji Ambresh Rajkumar
+ */
+public class T25 extends RefactoringTest {
+
+	@Override
+	protected String getTestFileName() {
+		return "MoveImportsTestFile";
+	}
+
+	@Override
+	protected void doExecuteRefactoring() throws Exception {
+
+		final String destinationForImports= "DestinationFile";
+		bot.createANewJavaClass(getProjectName(), destinationForImports);
+		bot.sleep();
+		bot.selectFromPackageExplorer(getProjectName(), "src", CodingSpectatorBot.PACKAGE_NAME, getTestFileFullName());
+		bot.getBot().menu("Navigate").menu("Open").click();
+		final String selection= "import java.util.ArrayList;\n" +
+										"import java.util.Queue;";
+		bot.selectElementToRefactor(getTestFileFullName(), 5, 0, selection.length());
+		bot.invokeRefactoringFromMenu("Move...");
+		bot.activateShellWithName("Textual Move");
+		bot.getCurrentTree().expandNode(getProjectName(), "src", CodingSpectatorBot.PACKAGE_NAME, getTestFileFullName());
+		bot.getCurrentTree().pressShortcut(org.eclipse.jface.bindings.keys.KeyStroke.getInstance('D'));
+		bot.clickButtons(IDialogConstants.CANCEL_LABEL);
+	}
+}
