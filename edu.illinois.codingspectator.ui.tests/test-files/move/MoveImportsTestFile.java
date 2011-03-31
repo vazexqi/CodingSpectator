703248e78ef0bf1d56cc08959cfc48231f46694e
Balaji Ambresh Rajkumar <rockmylife@gmail.com>
2011-03-30 23:59:42 -0400

    Added the test input file for MoveImportDeclarationsPolicy.
    This commit belongs to issue #124.

---
+++ edu.illinois.codingspectator.ui.tests/test-files/move/MoveImportsTestFile.java
@@ -0,0 +1,11 @@
+/**
+ * This file is licensed under the University of Illinois/NCSA Open Source License. See LICENSE.TXT for details.
+ */
+package edu.illinois.codingspectator;
+
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.Set;
+
+public class MoveImportsTestFile {
+}
