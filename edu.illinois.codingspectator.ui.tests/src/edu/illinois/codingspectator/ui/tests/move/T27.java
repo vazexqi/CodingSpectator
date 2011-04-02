926650a78f19f50d59e1fbd16d930176483ede35
Balaji Ambresh Rajkumar <rockmylife@gmail.com>
2011-04-02 12:46:02 -0400

    Added tests for perform and cancel refactorings of MoveMembersPolicy.
    This commit belongs to issue #124.

---
+++ edu.illinois.codingspectator.ui.tests/src/edu/illinois/codingspectator/ui/tests/move/T27.java
@@ -0,0 +1,40 @@
+/**
+ * This file is licensed under the University of Illinois/NCSA Open Source License. See LICENSE.TXT for details.
+ */
+package edu.illinois.codingspectator.ui.tests.move;
+
+import org.eclipse.jface.dialogs.IDialogConstants;
+import edu.illinois.codingspectator.ui.tests.CodingSpectatorBot;
+import edu.illinois.codingspectator.ui.tests.RefactoringTest;
+
+/**
+ * This tests exercises the MoveMembersPolicy processor. It cancels the move of 
+ * multiple instance variables from one class to another.
+ * 
+ * @author Balaji Ambresh Rajkumar
+ */
+public class T27 extends RefactoringTest {
+
+	@Override
+	protected String getTestFileName() {
+		return "MoveInstanceMembersTestFile";
+	}
+
+	@Override
+	protected void doExecuteRefactoring() throws Exception {
+
+		final String destinationForImports= "DestinationFile";
+		bot.createANewJavaClass(getProjectName(), destinationForImports);
+		bot.sleep();
+		bot.selectFromPackageExplorer(getProjectName(), "src", CodingSpectatorBot.PACKAGE_NAME, getTestFileFullName());
+		bot.getBot().menu("Navigate").menu("Open").click();
+		final String selection= "    private int i;\n" +
+				"    private int j;";
+		bot.selectElementToRefactor(getTestFileFullName(), 7, 4, selection.length());
+		bot.invokeRefactoringFromMenu("Move...");
+		bot.activateShellWithName("Textual Move");
+		bot.getCurrentTree().expandNode(getProjectName(), "src", CodingSpectatorBot.PACKAGE_NAME, getTestFileFullName());
+		bot.getCurrentTree().pressShortcut(org.eclipse.jface.bindings.keys.KeyStroke.getInstance('D'));
+		bot.clickButtons(IDialogConstants.CANCEL_LABEL);
+	}
+}
