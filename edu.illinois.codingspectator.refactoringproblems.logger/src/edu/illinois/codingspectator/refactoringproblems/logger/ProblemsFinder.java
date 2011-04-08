f68604b3e22b5fb8ce1d45e8d1e11889fc895db4
Nick Chen <nchen@illinois.edu>
2011-04-07 19:03:47 -0500

    Report problem changes between pre and post refactoring
    Significant changes:
    
    * Added hashCode() and equals() so we can compare IProblems
    * Stored a previous/current problems for diffing after refactoring
    * Called CompilationUnit#becomeWorkingCopy to prevent the reconciler
    operation from complaining that a file does not exist.
    
    Problems:
    
    * IProblem keeps track of its location of the problem in the code. After
    a refactoring is performed, some of the problems "shift" around to
    different lines; this means that our diffing method does not work to
    identify changed problems.
    
    Proposed solution:
    
    * Do a symmetric set diff both ways A\B and B\A so we can keep track of
    problems introduced and removed by a refactoring.
    * We still have to use manual inspection to see if two problems "match"
    before and after the refactoring.
    
    This commit belongs to issue #191.

---
+++ edu.illinois.codingspectator.refactoringproblems.logger/src/edu/illinois/codingspectator/refactoringproblems/logger/ProblemsFinder.java
@@ -0,0 +1,183 @@
+/**
+ * This file is licensed under the University of Illinois/NCSA Open Source License. See LICENSE.TXT for details.
+ */
+package edu.illinois.codingspectator.refactoringproblems.logger;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.core.compiler.CategorizedProblem;
+import org.eclipse.jdt.internal.compiler.problem.DefaultProblem;
+import org.eclipse.jdt.internal.compiler.problem.ProblemSeverities;
+import org.eclipse.jdt.internal.core.CompilationUnit;
+import org.eclipse.jdt.internal.core.DefaultWorkingCopyOwner;
+import org.eclipse.jdt.internal.core.JavaModelManager;
+import org.eclipse.jdt.internal.core.ReconcileWorkingCopyOperation;
+
+/**
+ * 
+ * @author Balaji Ambresh Rajkumar
+ * @author Mohsen Vakilian
+ * @author nchen
+ * @author Stas Negara
+ * 
+ */
+@SuppressWarnings("restriction")
+public class ProblemsFinder {
+
+	Set<Map<String, DefaultProblemWrapper[]>> problems;
+
+	Set<CompilationUnit> affectedCompilationUnits;
+
+	/**
+	 * 
+	 * This method is based on org.eclipse.jdt.internal.core.CompilationUnit#reconcile(int, int,
+	 * WorkingCopyOwner, IProgressMonitor)
+	 * 
+	 * @param compilationUnit
+	 * @throws JavaModelException
+	 */
+	@SuppressWarnings("unchecked")
+	private void computeProblems(CompilationUnit compilationUnit) throws JavaModelException {
+		ReconcileWorkingCopyOperation op= new ReconcileWorkingCopyOperation(compilationUnit, CompilationUnit.NO_AST, ICompilationUnit.FORCE_PROBLEM_DETECTION, DefaultWorkingCopyOwner.PRIMARY);
+		JavaModelManager manager= JavaModelManager.getJavaModelManager();
+		try {
+			manager.cacheZipFiles(this); // cache zip files for performance (see https://bugs.eclipse.org/bugs/show_bug.cgi?id=134172)
+			op.runOperation(new NullProgressMonitor());
+			Map<String, CategorizedProblem[]> CUJavaProblems= op.problems;
+			problems.add(convertRepresentationOfProblems(CUJavaProblems));
+		} finally {
+			manager.flushZipFiles(this);
+		}
+	}
+
+	public Set<Map<String, DefaultProblemWrapper[]>> computeProblems(Set<CompilationUnit> affectedCompilationUnits) throws JavaModelException {
+		this.affectedCompilationUnits= affectedCompilationUnits;
+
+		problems= new HashSet<Map<String, DefaultProblemWrapper[]>>();
+		for (CompilationUnit compilationUnit : affectedCompilationUnits) {
+			computeProblems(compilationUnit);
+		}
+
+		return problems;
+	}
+
+
+	private static Map<String, DefaultProblemWrapper[]> convertRepresentationOfProblems(Map<String, CategorizedProblem[]> cUJavaProblems) {
+		Map<String, DefaultProblemWrapper[]> convertedRepresentations= new HashMap<String, DefaultProblemWrapper[]>();
+		for (String key : cUJavaProblems.keySet()) {
+			CategorizedProblem[] defaultProblems= cUJavaProblems.get(key);
+			DefaultProblemWrapper[] convertedRepresentation= DefaultProblemWrapper.initializeFromArrays(defaultProblems);
+			convertedRepresentations.put(key, convertedRepresentation);
+		}
+		return convertedRepresentations;
+	}
+}
+
+/**
+ * 
+ * Wraps the DefaultProblem type so that we can implement equals and hashCode for performing set
+ * operations
+ * 
+ */
+class DefaultProblemWrapper {
+	private char[] fileName;
+
+	private String message;
+
+	private int id;
+
+	private String[] arguments;
+
+	private int endPosition;
+
+	private int line;
+
+	private int startPosition;
+
+	private int severity;
+
+	@SuppressWarnings("restriction")
+	public DefaultProblemWrapper(CategorizedProblem problem) {
+		fileName= problem.getOriginatingFileName();
+		message= problem.getMessage();
+		id= problem.getID();
+		arguments= problem.getArguments();
+		endPosition= problem.getSourceEnd();
+		line= problem.getSourceLineNumber();
+		startPosition= problem.getSourceStart();
+		severity= problem.isError() ? ProblemSeverities.Error : ProblemSeverities.Warning;
+	}
+
+	public static DefaultProblemWrapper[] initializeFromArrays(CategorizedProblem[] categorizedProblems) {
+		DefaultProblemWrapper[] wrappers= new DefaultProblemWrapper[categorizedProblems.length];
+		for (int i= 0; i < categorizedProblems.length; i++) {
+			wrappers[i]= new DefaultProblemWrapper(categorizedProblems[i]);
+		}
+		return wrappers;
+	}
+
+	/////////////////////////////////
+	// Auto-generated through Eclipse
+	/////////////////////////////////
+
+	@Override
+	public int hashCode() {
+		final int prime= 31;
+		int result= 1;
+		result= prime * result + Arrays.hashCode(arguments);
+		result= prime * result + endPosition;
+		result= prime * result + Arrays.hashCode(fileName);
+		result= prime * result + id;
+		result= prime * result + line;
+		result= prime * result + ((message == null) ? 0 : message.hashCode());
+		result= prime * result + severity;
+		result= prime * result + startPosition;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DefaultProblemWrapper other= (DefaultProblemWrapper)obj;
+		if (!Arrays.equals(arguments, other.arguments))
+			return false;
+		if (endPosition != other.endPosition)
+			return false;
+		if (!Arrays.equals(fileName, other.fileName))
+			return false;
+		if (id != other.id)
+			return false;
+		if (line != other.line)
+			return false;
+		if (message == null) {
+			if (other.message != null)
+				return false;
+		} else if (!message.equals(other.message))
+			return false;
+		if (severity != other.severity)
+			return false;
+		if (startPosition != other.startPosition)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "DefaultProblemWrapper [fileName=" + Arrays.toString(fileName) + ", message=" + message + ", id=" + id + ", arguments=" + Arrays.toString(arguments) + ", endPosition=" + endPosition
+				+ ", line=" + line + ", startPosition=" + startPosition + ", severity=" + severity + "]";
+	}
+	
+	
+}
--- edu.illinois.codingspectator.refactoringproblems.logger/src/edu/illinois/codingspectator/refactoringproblems/logger/RefactoringProblemsListener.java
+++ edu.illinois.codingspectator.refactoringproblems.logger/src/edu/illinois/codingspectator/refactoringproblems/logger/RefactoringProblemsListener.java
@@ -4,8 +4,9 @@
 package edu.illinois.codingspectator.refactoringproblems.logger;
 
 
-import java.util.Collection;
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 import org.eclipse.core.commands.operations.IOperationHistoryListener;
@@ -13,8 +14,13 @@ import org.eclipse.core.commands.operations.IUndoableOperation;
 import org.eclipse.core.commands.operations.OperationHistoryEvent;
 import org.eclipse.core.commands.operations.OperationHistoryFactory;
 import org.eclipse.core.commands.operations.TriggeredOperations;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.jdt.core.IJavaModelMarker;
 import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.internal.core.CompilationUnit;
+import org.eclipse.ltk.core.refactoring.RefactoringCore;
 import org.eclipse.ltk.core.refactoring.history.IRefactoringExecutionListener;
 import org.eclipse.ltk.core.refactoring.history.RefactoringExecutionEvent;
 import org.eclipse.ltk.internal.core.refactoring.UndoableOperation2ChangeAdapter;
@@ -23,7 +29,8 @@ import org.eclipse.ui.IStartup;
 /**
  * 
  * This class is based on
- * edu.illinois.codingspectator.codingtracker.listeners.OperationHistoryListener
+ * edu.illinois.codingspectator.codingtracker.listeners.OperationHistoryListener and
+ * edu.illinois.codingspectator.codingtracker.listeners.RefactoringExecutionListener
  * 
  * @author Balaji Ambresh Rajkumar
  * @author Mohsen Vakilian
@@ -34,25 +41,45 @@ import org.eclipse.ui.IStartup;
 @SuppressWarnings("restriction")
 public class RefactoringProblemsListener implements IStartup, IOperationHistoryListener, IRefactoringExecutionListener {
 
-	ProblemFinder problemFinder;
-	
+	private ProblemsFinder problemsFinder;
+
+	private ProblemsComparer problemsComparer;
+
+	private Set<CompilationUnit> affectedCompilationUnits;
+
+	public RefactoringProblemsListener() {
+		problemsFinder= new ProblemsFinder();
+		problemsComparer= new ProblemsComparer();
+	}
+
 	@Override
 	public void earlyStartup() {
-		OperationHistoryFactory.getOperationHistory().addOperationHistoryListener(new RefactoringProblemsListener());
+		OperationHistoryFactory.getOperationHistory().addOperationHistoryListener(this);
+		RefactoringCore.getHistoryService().addExecutionListener(this);
 	}
 
 	@Override
 	public void historyNotification(OperationHistoryEvent event) {
-		if (isAboutEvent(event)) {
-			Set<CompilationUnit> affectedCompilationUnits= getAffectedCompilationUnits(event);
-			problemFinder= new ProblemFinder(affectedCompilationUnits);
+		if (isAboutToRefactor(event)) {
+			affectedCompilationUnits= getAffectedCompilationUnits(event);
 			try {
-				problemFinder.computeProblems();
+				storeCurrentProblems();
 			} catch (JavaModelException e) {
-				//FIXME
-				e.printStackTrace();
+				Activator.getDefault().getLog().log(new Status(IStatus.WARNING, Activator.PLUGIN_ID, "CODINGSPECTATOR: Failed to log compilation problems", e));
 			}
 		}
+	}
+
+	private void makeAffectedCompilationUnitsBecomeWorkingCopies() throws JavaModelException {
+		for (CompilationUnit cu : affectedCompilationUnits) {
+			cu.becomeWorkingCopy(new NullProgressMonitor());
+		}
+	}
+
+	private void storeCurrentProblems() throws JavaModelException {
+		makeAffectedCompilationUnitsBecomeWorkingCopies();
+		Set<Map<String, DefaultProblemWrapper[]>> computeProblems= problemsFinder.computeProblems(affectedCompilationUnits);
+		problemsComparer.pushNewProblemsSet(computeProblems);
 	}
 
 	private Set<CompilationUnit> getAffectedCompilationUnits(OperationHistoryEvent event) {
@@ -74,7 +101,7 @@ public class RefactoringProblemsListener implements IStartup, IOperationHistoryL
 		return affectedCompilationUnits;
 	}
 
-	private boolean isAboutEvent(OperationHistoryEvent event) {
+	private boolean isAboutToRefactor(OperationHistoryEvent event) {
 		int eventType= event.getEventType();
 
 		return eventType == OperationHistoryEvent.ABOUT_TO_EXECUTE || (eventType == OperationHistoryEvent.ABOUT_TO_REDO) ||
@@ -85,10 +112,11 @@ public class RefactoringProblemsListener implements IStartup, IOperationHistoryL
 	public void executionNotification(RefactoringExecutionEvent event) {
 		if (isRefactoringPerformedEvent(event)) {
 			try {
-				problemFinder.computeProblems();
+				storeCurrentProblems();
+				Set<DefaultProblemWrapper> compareProblems= problemsComparer.compareProblems();
+				System.err.println(compareProblems);
 			} catch (JavaModelException e) {
-				//FIXME
-				e.printStackTrace();
+				Activator.getDefault().getLog().log(new Status(IStatus.WARNING, Activator.PLUGIN_ID, "CODINGSPECTATOR: Failed to log compilation problems", e));
 			}
 		}
 	}
@@ -98,4 +126,27 @@ public class RefactoringProblemsListener implements IStartup, IOperationHistoryL
 		return (eventType == RefactoringExecutionEvent.PERFORMED || eventType == RefactoringExecutionEvent.REDONE || eventType == RefactoringExecutionEvent.UNDONE);
 	}
 
+}
+
+class ProblemsComparer {
+
+	private Set<Map<String, DefaultProblemWrapper[]>> previousProblems;
+
+	private Set<Map<String, DefaultProblemWrapper[]>> currentProblems;
+
+	public void pushNewProblemsSet(Set<Map<String, DefaultProblemWrapper[]>> problems) {
+		previousProblems= currentProblems;
+		currentProblems= problems;
+	}
+
+	public Set<DefaultProblemWrapper> compareProblems() {
+		Set<DefaultProblemWrapper> reportedRefactoringProblems= new HashSet<DefaultProblemWrapper>();
+		currentProblems.removeAll(previousProblems);
+		for (Map<String, DefaultProblemWrapper[]> newProblemsFromPerformedRefactoring : currentProblems) {
+			DefaultProblemWrapper[] defaultProblemWrappers= newProblemsFromPerformedRefactoring.get((IJavaModelMarker.JAVA_MODEL_PROBLEM_MARKER));
+			reportedRefactoringProblems.addAll(Arrays.asList(defaultProblemWrappers));
+		}
+
+		return reportedRefactoringProblems;
+	}
 }
