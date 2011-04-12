ff30e1562aecd1022c806f89bb204036d84b42e0
Nick Chen <nchen@illinois.edu>
2011-04-12 16:57:46 -0500

    Removed duplication on CodeSnippet in WatchedJavaRefactoring
    In our previous commit we forgot to use the new CodeSnippetExtractor that we created.
    
    This commit belongs to issue #170.

--- org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/corext/refactoring/codingspectator/WatchedJavaRefactoring.java
+++ org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/corext/refactoring/codingspectator/WatchedJavaRefactoring.java
@@ -3,8 +3,6 @@ package org.eclipse.jdt.internal.corext.refactoring.codingspectator;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.eclipse.core.runtime.NullProgressMonitor;
-
 import org.eclipse.ltk.core.refactoring.Refactoring;
 import org.eclipse.ltk.core.refactoring.RefactoringDescriptor;
 import org.eclipse.ltk.core.refactoring.RefactoringStatus;
@@ -14,13 +12,6 @@ import org.eclipse.ltk.core.refactoring.codingspectator.Logger;
 
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.ITypeRoot;
-import org.eclipse.jdt.core.dom.ASTNode;
-import org.eclipse.jdt.core.dom.NodeFinder;
-
-import org.eclipse.jdt.internal.corext.dom.ASTNodes;
-import org.eclipse.jdt.internal.corext.refactoring.util.RefactoringASTParser;
-
-import org.eclipse.jdt.internal.ui.JavaPlugin;
 
 /**
  * This class serves as the base class for all refactorings that we instrument in JDT. It has a
@@ -31,9 +22,6 @@ import org.eclipse.jdt.internal.ui.JavaPlugin;
  * 
  */
 public abstract class WatchedJavaRefactoring extends Refactoring implements IWatchedRefactoring {
-
-
-	private static final String DEFAULT_NULL_ASTNODE_CODE_SNIPPET= "EMPTY CODE SNIPPET"; //$NON-NLS-1$
 
 	protected int fSelectionStart;
 
@@ -51,8 +39,6 @@ public abstract class WatchedJavaRefactoring extends Refactoring implements IWat
 
 	protected Map populateInstrumentationData(RefactoringStatus refactoringStatus) {
 		Map arguments= new HashMap();
-		arguments.put(RefactoringDescriptor.ATTRIBUTE_CODE_SNIPPET, getCodeSnippet());
-		arguments.put(RefactoringDescriptor.ATTRIBUTE_SELECTION_TEXT, getSelectionText());
 		arguments.put(RefactoringDescriptor.ATTRIBUTE_STATUS, refactoringStatus.toString());
 		arguments.put(RefactoringDescriptor.ATTRIBUTE_INVOKED_BY_QUICKASSIST, String.valueOf(isInvokedByQuickAssist()));
 		getCodeSnippetInformation().insertIntoMap(arguments);
@@ -68,38 +54,6 @@ public abstract class WatchedJavaRefactoring extends Refactoring implements IWat
 		if (javaProject != null)
 			project= javaProject.getElementName();
 		return project;
-	}
-
-	protected String getSelectionText() {
-		try {
-			return getJavaTypeRoot().getBuffer().getText(fSelectionStart, fSelectionLength);
-		} catch (Exception e) {
-			JavaPlugin.log(e);
-		}
-		return null;
-	}
-
-	private String getCodeSnippet() {
-		ASTNode node= findTargetNode();
-
-		if (node == null) {
-			return DEFAULT_NULL_ASTNODE_CODE_SNIPPET;
-		}
-
-		final int THRESHOLD= 3200;
-
-		while (node.getParent() != null && node.subtreeBytes() < THRESHOLD) {
-			node= node.getParent();
-		}
-
-		return ASTNodes.asFormattedString(node, 4, System.getProperty("line.separator"), getJavaTypeRoot().getJavaProject().getOptions(true)); //$NON-NLS-1$
-	}
-
-	private ASTNode findTargetNode() {
-		ASTNode localNode= RefactoringASTParser.parseWithASTProvider(getJavaTypeRoot(), false, new NullProgressMonitor());
-
-		// see (org.eclipse.jdt.internal.corext.refactoring.code.ExtractMethodRefactoring.checkInitialConditions(IProgressMonitor))
-		return NodeFinder.perform(localNode, fSelectionStart, fSelectionLength);
 	}
 
 	abstract protected ITypeRoot getJavaTypeRoot();
