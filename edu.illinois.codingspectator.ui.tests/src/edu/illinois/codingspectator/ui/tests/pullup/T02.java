4001b1b2f80dbed269618bb7b8ec7cb5a851c6c4
Mohsen Vakilian <reprogrammer@gmail.com>
2011-05-19 19:58:04 -0500

    Fixed tests pullup.(T01,T02) and pushdown.T01.
    Note that the tests pullup.(T01,T02) used to throw exceptions.
    
    This commit is related to issues #220 and #22.

--- edu.illinois.codingspectator.ui.tests/src/edu/illinois/codingspectator/ui/tests/pullup/T02.java
+++ edu.illinois.codingspectator.ui.tests/src/edu/illinois/codingspectator/ui/tests/pullup/T02.java
@@ -4,7 +4,6 @@
 package edu.illinois.codingspectator.ui.tests.pullup;
 
 import org.eclipse.jface.dialogs.IDialogConstants;
-import org.eclipse.swtbot.swt.finder.exceptions.WidgetNotFoundException;
 
 import edu.illinois.codingspectator.ui.tests.RefactoringTest;
 
@@ -28,14 +27,7 @@ public class T02 extends RefactoringTest {
 	protected void doExecuteRefactoring() {
 		bot.selectElementToRefactor(getTestFileFullName(), 14, 9, "m".length());
 		bot.invokeRefactoringFromMenu(PULL_UP_MENU_ITEM);
-		bot.clickButtons(IDialogConstants.FINISH_LABEL);
-		try {
-			bot.clickButtons(IDialogConstants.OK_LABEL);
-		} catch (WidgetNotFoundException exception) {
-			// FIXME: On my machine i.e. Mac, the second dialog box does not appear so SWTBot can't click on OK.
-			bot.clickButtons(IDialogConstants.FINISH_LABEL);
-		}
-		printMessage("Eclipse (with or without CodingSpectator) fails to perform this refactoring and doesn't log it. This test makes Eclipse throw the following exceptions: java.lang.reflect.InvocationTargetException\nCaused by: java.lang.NullPointerException\nRoot exception:\njava.lang.NullPointerException");
+		bot.clickButtons(IDialogConstants.FINISH_LABEL, IDialogConstants.FINISH_LABEL);
 	}
 
 }
