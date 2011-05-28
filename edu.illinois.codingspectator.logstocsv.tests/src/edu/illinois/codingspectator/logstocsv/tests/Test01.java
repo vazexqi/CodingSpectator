e4ac0b330863a9719e27abdd500a3cf48f602499
Mohsen Vakilian <reprogrammer@gmail.com>
2011-05-28 16:12:22 -0500

    Made the test campare the actual and expected results.
    This commit belongs to issue #230.

--- edu.illinois.codingspectator.logstocsv.tests/src/edu/illinois/codingspectator/logstocsv/tests/Test01.java
+++ edu.illinois.codingspectator.logstocsv.tests/src/edu/illinois/codingspectator/logstocsv/tests/Test01.java
@@ -3,6 +3,7 @@
  */
 package edu.illinois.codingspectator.logstocsv.tests;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
@@ -13,6 +14,7 @@ import org.eclipse.core.runtime.Path;
 import org.junit.Test;
 
 import edu.illinois.codingspectator.efs.EFSFile;
+import edu.illinois.codingspectator.file.utils.FileUtils;
 import edu.illinois.codingspectator.logstocsv.ConvertLogsToCSV;
 
 /**
@@ -26,13 +28,14 @@ public class Test01 {
 	@Test
 	public void test() throws CoreException, IOException {
 		IPath pathToTestFolder= new Path("resources").append("01");
+		IPath csvExpectedLog= pathToTestFolder.append("expected-output").append("logs.csv");
 		IPath pathToInputFolder= pathToTestFolder.append("input");
 		EFSFile csvActualLogFolder= new EFSFile(pathToTestFolder.append("actual-output"));
 		csvActualLogFolder.mkdir();
 		EFSFile csvActualLog= new EFSFile(csvActualLogFolder.getPath().append("logs.csv"));
 		ConvertLogsToCSV.main(new String[] { null, pathToInputFolder.toOSString(), csvActualLog.getPath().toOSString() });
 		assertTrue(csvActualLog.exists());
-		//FIXME: Compare the actual and expected log files.
+		assertEquals(FileUtils.getContents(csvExpectedLog.toOSString()), FileUtils.getContents(csvActualLog.getPath().toOSString()));
 		csvActualLogFolder.delete();
 	}
 
