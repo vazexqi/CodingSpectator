03c6876b8c1bad03d2ddca0cd855638d66aa41b0
Mohsen Vakilian <reprogrammer@gmail.com>
2011-07-06 10:19:22 -0500

    Consulted the update site matching the version of Eclipse.
    Made the updater consult the CodingSpectator's update site for Helios if the running Eclipse is Helios and CodingSpectator's update site for Indigo if the running Eclipse is Indigo.
    
    This commit belongs to issue #245.

--- plug-ins/edu.illinois.codingspectator.monitor.ui/src/edu/illinois/codingspectator/monitor/ui/updates/Updater.java
+++ plug-ins/edu.illinois.codingspectator.monitor.ui/src/edu/illinois/codingspectator/monitor/ui/updates/Updater.java
@@ -3,6 +3,13 @@
  */
 package edu.illinois.codingspectator.monitor.ui.updates;
 
+import org.eclipse.core.runtime.Assert;
+import org.eclipse.core.runtime.AssertionFailedException;
+import org.eclipse.core.runtime.Platform;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.Version;
+
+import edu.illinois.bundleupdater.Activator;
 import edu.illinois.bundleupdater.BundleUpdater;
 import edu.illinois.codingspectator.monitor.core.submission.SubmitterListener;
 
@@ -24,6 +31,52 @@ public class Updater implements SubmitterListener {
 	 */
 	@Override
 	public void postSubmit(boolean succeeded) {
-		new BundleUpdater("http://codingspectator.cs.illinois.edu/updates/", "edu.illinois.codingspectator.feature.group").checkForUpdates();
+		if (isEclipseVersionIsSupported()) {
+			new BundleUpdater(getUpdateSiteURL(), "edu.illinois.codingspectator.feature.group").checkForUpdates();
+		} else {
+			try {
+				Assert.isTrue(false);
+			} catch (AssertionFailedException e) {
+				Activator.getDefault().logErrorStatus("Could not detect the Eclipse release name while trying to find the update site.", e);
+			}
+		}
 	}
+
+	private boolean isEclipseVersionIsSupported() {
+		return isHelios() || isIndigo();
+	}
+
+	private String getUpdateSiteURL() {
+		String baseURL= "http://codingspectator.cs.illinois.edu/updates/";
+		if (isHelios()) {
+			return baseURL + "helios";
+		} else if (isIndigo()) {
+			return baseURL + "indigo";
+		} else {
+			throw new RuntimeException("Unsupported Eclipse detected.");
+		}
+	}
+
+	/**
+	 * See org.eclipse.ui.internal.ProductInfo#getAppVersion()
+	 * 
+	 * @return
+	 */
+	private Version getEclipseVersion() {
+		Bundle bundle= Platform.getBundle("org.eclipse.ui");
+		if (bundle == null) {
+			return Version.emptyVersion;
+		} else {
+			return bundle.getVersion();
+		}
+	}
+
+	private boolean isHelios() {
+		return getEclipseVersion().getMajor() == 3 && getEclipseVersion().getMinor() == 6;
+	}
+
+	private boolean isIndigo() {
+		return getEclipseVersion().getMajor() == 3 && getEclipseVersion().getMinor() == 7;
+	}
+
 }
