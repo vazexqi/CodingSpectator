ec5e584eea5d5f773a882c2f1d1fb770b4d423ec
Mohsen Vakilian <reprogrammer@gmail.com>
2011-07-09 22:53:30 -0500

    Updated the test files of encapsulatefield.T02 and added encapsulatefield.T05.
    Confirmed that the wrong expected files are because of a bug in SWTBot tests and not CodingSpectator.
    
    This commit belongs to issue #275.

---
+++ plug-ins/edu.illinois.codingspectator.ui.tests/src/edu/illinois/codingspectator/ui/tests/encapsulatefield/T05.java
@@ -0,0 +1,38 @@
+/**
+ * This file is licensed under the University of Illinois/NCSA Open Source License. See LICENSE.TXT for details.
+ */
+package edu.illinois.codingspectator.ui.tests.encapsulatefield;
+
+import org.eclipse.jface.dialogs.IDialogConstants;
+
+import edu.illinois.codingspectator.ui.tests.CodingSpectatorBot;
+import edu.illinois.codingspectator.ui.tests.RefactoringTest;
+
+/**
+ * 
+ * This test invokes an encapsulate field refactoring using a structured selection, then changes the
+ * selection of a radio box and performs the refactoring.
+ * 
+ * @author Mohsen Vakilian
+ * 
+ */
+public class T05 extends RefactoringTest {
+
+	private static final String MENU_ITEM= "Encapsulate Field...";
+
+	private static final String SELECTION= "field";
+
+	@Override
+	protected String getTestFileName() {
+		return "EncapsulateFieldTestFile";
+	}
+
+	@Override
+	protected void doExecuteRefactoring() {
+		bot.selectFromPackageExplorer(getProjectName(), "src", CodingSpectatorBot.PACKAGE_NAME, getTestFileFullName(), getTestFileName(), SELECTION);
+		bot.invokeRefactoringFromMenu(MENU_ITEM);
+		bot.getBot().radio("keep field reference").click();
+		bot.clickButtons(IDialogConstants.OK_LABEL);
+	}
+
+}
