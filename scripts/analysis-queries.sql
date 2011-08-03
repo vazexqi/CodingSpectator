1f1f1db1f5c1f9b86e306f27f205ac4d188e865a
Mohsen Vakilian <reprogrammer@gmail.com>
2011-08-03 02:17:09 -0500

    Added a query to summarize the refactoring activities of each user.
    Added a query to compute the number of different kinds of refactoring invocations per user.
    
    This commit belongs to issue #288.

--- scripts/analysis-queries.sql
+++ scripts/analysis-queries.sql
@@ -1,5 +1,6 @@
 CREATE TABLE "PUBLIC"."RefactoringIDs" ("id" varchar(1000));
 
+--Compute the set of all captured refactoring ID's.
 INSERT INTO "PUBLIC"."RefactoringIDs" ("id")
 SELECT "PUBLIC"."EclipseRefactorings"."id" FROM "PUBLIC"."EclipseRefactorings"
 UNION 
@@ -9,6 +10,7 @@ SELECT "PUBLIC"."CancelledRefactorings"."id" FROM "PUBLIC"."CancelledRefactoring
 UNION 
 SELECT "PUBLIC"."UnavailableRefactorings"."id" FROM "PUBLIC"."UnavailableRefactorings";
 
+--Compute the number of each kind of refactoring invocation per refactoring ID for all users.
 SELECT
 "PUBLIC"."RefactoringIDs"."id",
 "PUBLIC"."EclipseRefactorings"."count" AS "ECLIPSE_COUNT",
@@ -20,4 +22,15 @@ FULL OUTER JOIN "PUBLIC"."EclipseRefactorings" ON ("PUBLIC"."RefactoringIDs"."id
 FULL OUTER JOIN "PUBLIC"."PerformedRefactorings" ON ("PUBLIC"."RefactoringIDs"."id" = "PUBLIC"."PerformedRefactorings"."id")
 FULL OUTER JOIN "PUBLIC"."CancelledRefactorings" ON ("PUBLIC"."RefactoringIDs"."id" = "PUBLIC"."CancelledRefactorings"."id")
 FULL OUTER JOIN "PUBLIC"."UnavailableRefactorings" ON ("PUBLIC"."RefactoringIDs"."id" = "PUBLIC"."UnavailableRefactorings"."id");
+
+--Compute the number of each kind of refactoring invocation per user.
+SELECT
+"PUBLIC"."AllData"."username" AS "USERNAME",
+COUNT(CASE "PUBLIC"."AllData"."refactoring kind" WHEN 'ECLIPSE' THEN 1 ELSE NULL END) AS "ECLIPSE_COUNT",
+COUNT(CASE "PUBLIC"."AllData"."refactoring kind" WHEN 'PERFORMED' THEN 1 ELSE NULL END) AS "PERFORMED_COUNT",
+COUNT(CASE "PUBLIC"."AllData"."refactoring kind" WHEN 'CANCELLED' THEN 1 ELSE NULL END) AS "CANCELLED_COUNT",
+COUNT(CASE "PUBLIC"."AllData"."refactoring kind" WHEN 'UNAVAILABLE' THEN 1 ELSE NULL END) AS "UNAVAILABLE_COUNT"
+FROM "PUBLIC"."AllData"
+GROUP BY "PUBLIC"."AllData"."username" 
+ORDER BY "PUBLIC"."AllData"."username"; 
 
