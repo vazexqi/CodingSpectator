497ba660f21715508bc081209b2984b5ecdc0830
Wanderer777 <negara2003@yahoo.com>
2011-08-24 19:29:42 -0700

    Externally changed files are deleted in order avoid problems with commits.
    An external change operation may be followed by a commit operation, which compares the committed file content against the file content in the workspace, if the file exists. Therefore, externally changed files should be always deleted (except in testing mode) to ensure that the following commit operations do not fail.

--- plug-ins/edu.illinois.codingtracker.operations/src/edu/illinois/codingtracker/operations/resources/ExternallyModifiedResourceOperation.java
+++ plug-ins/edu.illinois.codingtracker.operations/src/edu/illinois/codingtracker/operations/resources/ExternallyModifiedResourceOperation.java
@@ -3,9 +3,13 @@
  */
 package edu.illinois.codingtracker.operations.resources;
 
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.texteditor.AbstractDecoratedTextEditor;
 
+import edu.illinois.codingtracker.helpers.EditorHelper;
 import edu.illinois.codingtracker.operations.OperationLexer;
 import edu.illinois.codingtracker.operations.OperationSymbols;
 import edu.illinois.codingtracker.operations.OperationTextChunk;
@@ -58,7 +62,21 @@ public class ExternallyModifiedResourceOperation extends ResourceOperation {
 	@Override
 	public void replay() throws CoreException {
 		IResource resource= findResource();
-		if (resource != null && isDeleted) {
+		//If not in test mode, delete the resource regardless of whether it was deleted or just modified since this operation 
+		//could be followed by a commit operation, whose replay would fail due to the wrong file content.
+		if (resource != null && (isDeleted || !isInTestMode)) {
+			//If deleted resource is opened in the active editor, close it first in order to avoid confusing the replayer 
+			//that tracks the current active editor.
+			IEditorPart activeEditor= EditorHelper.getActiveEditor();
+			if (activeEditor instanceof AbstractDecoratedTextEditor) {
+				IFile editedFile= EditorHelper.getEditedJavaFile((AbstractDecoratedTextEditor)activeEditor);
+				if (resource.equals(editedFile)) {
+					if (activeEditor == currentEditor) {
+						currentEditor= null;
+					}
+					activeEditor.getSite().getPage().closeEditor(activeEditor, false);
+				}
+			}
 			resource.delete(true, null);
 		}
 	}
