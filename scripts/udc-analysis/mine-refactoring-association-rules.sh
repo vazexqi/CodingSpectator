f36b989476f1c4b2d53c33cfc1e5723afa816cbd
Mohsen Vakilian <reprogrammer@gmail.com>
2011-11-22 14:40:56 -0600

    Automated the process of mining association rules from UDC data.
    Updated the csvtotransactions program to read from stdin and write into stdout. This model fits better into redirection and piping of shell scripts.
    Added the scripts for converting the UDC refactoring data to transactions and finally association rules.
    
    This commit belongs to issue #192.

---
+++ scripts/udc-analysis/mine-refactoring-association-rules.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# This file is licensed under the University of Illinois/NCSA Open Source License. See LICENSE.TXT for details.
+
+TEMP_UDC_REFACTORING_CSV_FILE=$(mktemp) || { echo "Failed to create temp file"; exit 1; }
+CSV_TO_TRANSACTIONS_FOLDER="$CODINGSPECTATOR_GIT_FOLDER/plug-ins/edu.illinois.codingspectator.csvtotransactions"
+TIMESTAMPED_UDC_DATA_FOLDER="$CODINGSPECTATOR_SVN_FOLDER/Experiment/UDCData/TimestampedUDCData/"
+CSV_TO_TRANSACTIONS=(java -XX:MaxPermSize=512m -Xms40m -Xmx1G -jar "$CSV_TO_TRANSACTIONS_FOLDER/ant/bin/csvtotransactions.jar")
+TRANSACTIONS_TO_RULES="$CODINGSPECTATOR_GIT_FOLDER/scripts/udc-analysis/transactionstorules.R"
+ant -f "$CSV_TO_TRANSACTIONS_FOLDER/ant/build.xml"
+tar xfz "$TIMESTAMPED_UDC_DATA_FOLDER/udc-refactoring-data.csv.tar.gz" --to-stdout > "$TEMP_UDC_REFACTORING_CSV_FILE"
+for time_window in 1 2 5
+do
+  "${CSV_TO_TRANSACTIONS[@]}" -t "$time_window" < "$TEMP_UDC_REFACTORING_CSV_FILE" | "$TRANSACTIONS_TO_RULES" > "$TIMESTAMPED_UDC_DATA_FOLDER/udc-refactoring-association-rules-t-$time_window.csv"
+done
+
+rm -rf "$TEMP_UDC_REFACTORING_CSV_FILE"
+
---
+++ scripts/udc-analysis/transactionstorules.R
@@ -0,0 +1,19 @@
+#!/usr/bin/Rscript --vanilla
+# This file is licensed under the University of Illinois/NCSA Open Source License. See LICENSE.TXT for details.
+
+logcon <- file("R.log", open = "wt")
+sink(file = logcon, type = "output")
+sink(file = logcon, type = "message")
+library(package = "arules")
+transactions = read.transactions(file = file("stdin"), format = "basket", sep = ",")
+#frequencies = itemFrequency(transactions)
+#cat(frequencies)
+#WRITE(frequencies, file = "tx.csv", sep = ",")
+rules = apriori(transactions, parameter = list(minlen=2, sup = 0.001, conf = 0.001))
+summary(rules)
+inspect(rules)
+sink(file = NULL, type = "output")
+sink(file = NULL, type = "message")
+close(logcon)
+WRITE(rules, file = "", sep = ",", quote = TRUE, col.names = NA)
+
