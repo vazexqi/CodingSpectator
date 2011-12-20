81e750ad90b83769606c77a309bf60db56585de7
Mohsen Vakilian <reprogrammer@gmail.com>
2011-12-20 15:10:48 -0600

    Added a step to filter some events from association rule mining.

--- scripts/udc-analysis/mine-refactoring-association-rules.sh
+++ scripts/udc-analysis/mine-refactoring-association-rules.sh
@@ -6,8 +6,9 @@ CSV_TO_TRANSACTIONS_FOLDER="$CODINGSPECTATOR_GIT_FOLDER/plug-ins/edu.illinois.co
 TIMESTAMPED_UDC_DATA_FOLDER="$CODINGSPECTATOR_SVN_FOLDER/Experiment/UDCData/TimestampedUDCData/"
 CSV_TO_TRANSACTIONS=(java -XX:MaxPermSize=512m -Xms40m -Xmx1G -jar "$CSV_TO_TRANSACTIONS_FOLDER/ant/bin/csvtotransactions.jar")
 TRANSACTIONS_TO_RULES="$CODINGSPECTATOR_GIT_FOLDER/scripts/udc-analysis/transactionstorules.R"
+UDC_COMMAND_IDS_UNDER_STUDY="$CODINGSPECTATOR_GIT_FOLDER/scripts/udc-analysis/udc-refactoring-ids"
 ant -f "$CSV_TO_TRANSACTIONS_FOLDER/ant/build.xml"
-tar xfz "$TIMESTAMPED_UDC_DATA_FOLDER/udc-refactoring-data.csv.tar.gz" --to-stdout > "$TEMP_UDC_REFACTORING_CSV_FILE"
+tar xfz "$TIMESTAMPED_UDC_DATA_FOLDER/udc-refactoring-data.csv.tar.gz" --to-stdout | grep -E -f "$UDC_COMMAND_IDS_UNDER_STUDY" > "$TEMP_UDC_REFACTORING_CSV_FILE"
 for time_window in 1 2 5 10 30 60
 do
   "${CSV_TO_TRANSACTIONS[@]}" -t "$time_window" < "$TEMP_UDC_REFACTORING_CSV_FILE" | "$TRANSACTIONS_TO_RULES" > "$TIMESTAMPED_UDC_DATA_FOLDER/udc-refactoring-association-rules-t-$time_window.csv"
