7bf7dac4b1be1a9f73acf27bbcf5292b16817e25
Mohsen Vakilian <reprogrammer@gmail.com>
2012-01-22 20:01:46 -0600

    Added the script to plot programmers' use of refactorings.

---
+++ scripts/udc-analysis/draw-graphs.R
@@ -0,0 +1,29 @@
+#!/usr/bin/env Rscript
+# This file is licensed under the University of Illinois/NCSA Open Source License. See LICENSE.TXT for details.
+
+codingspectator_svn_folder <- Sys.getenv("CODINGSPECTATOR_SVN_FOLDER")
+udc_distributions_folder <- paste(codingspectator_svn_folder, "Experiment", "UDCData", "TimestampedUDCData", "Distributions", sep = "/")
+users_distinct_refactorings_csv <- paste(udc_distributions_folder, "users-distinct-refactorings.csv", sep = "/")
+table <- read.table(file = users_distinct_refactorings_csv, header = TRUE, sep = ",")
+X <- table$DISTINCT_REFACTORINGS
+Y <- table$USERS
+
+users_distinct_refactorings_png <- paste(udc_distributions_folder, "users-distinct-refactorings.png", sep = "/")
+png(filename = users_distinct_refactorings_png)
+plot(x = X, y = Y, type = "b", log = "y", xlab = "Number of Different Kinds of Refactorings Used", ylab = "Number of Programmers (log-scale)", main = "Diversity of Refactorings Used by Programmers", xaxt = "n", yaxt = "n")
+axis(1, at = X, labels = X)
+axis(2, at = 10^(0:log10(max(Y))), labels = 10^(0:log10(max(Y))))
+abline(v = X, col = 'grey', lwd = 0.5)
+abline(h = Y, col = 'grey', lwd = 0.5)
+dev.off()
+
+users_distinct_refactorings_cumulative_png <- paste(udc_distributions_folder, "users-distinct-refactorings-cumulative.png", sep = "/")
+png(filename = users_distinct_refactorings_cumulative_png)
+Z <- cumsum(table$USERS) / sum(table$USERS) * 100
+plot(x = X, y = Z, type = "b", xlab = "Maximum Number of Different Kinds of Refactorings Used", ylab = "Percentage of Programmers", main = "Diversity of Refactorings Used by Programmers", xaxt = "n", yaxt = "n")
+axis(1, at = X, labels = X)
+axis(2, at = Z[c(1:5, 23)], labels = sprintf("%.1f", Z[c(1:5, 23)]), las = 1)
+abline(v = X, col = 'grey', lwd = 0.5)
+abline(h = Z[c(1:5, 23)], col = 'grey', lwd = 0.5)
+dev.off()
+
