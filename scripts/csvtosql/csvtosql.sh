04873684592d9b91f6f1857f206951153294bc4f
Mohsen Vakilian <reprogrammer@gmail.com>
2012-02-18 23:01:40 -0600

    Linked the script to RefactorBehavior SVN.

--- scripts/csvtosql/csvtosql.sh
+++ scripts/csvtosql/csvtosql.sh
@@ -5,8 +5,13 @@ HSQLDB_FOLDER="$CODINGSPECTATOR_GIT_FOLDER/plug-ins/common/org.hsqldb/ant/downlo
 HSQLDB_JARS="$CODINGSPECTATOR_GIT_FOLDER/plug-ins/common/org.hsqldb/ant/downloads/hsqldb-2.2.6/hsqldb/lib/*.jar"
 CSVTOSQL_FOLDER="$CODINGSPECTATOR_GIT_FOLDER/plug-ins/common/edu.illinois.codingspectator.csvtosql"
 CSVTOSQL_JAR="$CSVTOSQL_FOLDER/ant/bin/csvtosql.jar"
+LOGS_CSV_FOLDER="$CODINGSPECTATOR_SVN_FOLDER/Experiment/Summer2011/InternalAndExternal/ICSE2012/Data/"
 
 ant -f "$CSVTOSQL_FOLDER/ant/build.xml"
-
-CS_CSV="$CODINGSPECTATOR_SVN_FOLDER/Experiment/Summer2011/InternalAndExternal/ICSE2012/Data/logs.csv" java -XX:MaxPermSize=1G -Xms1G -Xmx60G -cp $HSQLDB_JARS:$CSVTOSQL_JAR -jar $CSVTOSQL_JAR
+rm -f "$LOGS_CSV_FOLDER/logs.csv"
+tar -xOf "$LOGS_CSV_FOLDER/logs.csv.tar.gz" > "$LOGS_CSV_FOLDER/logs.csv"
+CS_CSV="$LOGS_CSV_FOLDER/logs.csv" java -XX:MaxPermSize=1G -Xms1G -Xmx60G -cp $HSQLDB_JARS:$CSVTOSQL_JAR -jar $CSVTOSQL_JAR
+rm "$LOGS_CSV_FOLDER/logs.csv"
+tar cfz "$LOGS_CSV_FOLDER/logs-hsqldb.tar.gz" db_file*
+rm db_file*
 
