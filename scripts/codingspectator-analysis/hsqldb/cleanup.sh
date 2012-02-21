81b43b7007f3336c3d3768086767e692a01d1ba1
Mohsen Vakilian <reprogrammer@gmail.com>
2012-02-20 23:35:12 -0600

    Removed symlinks of the input CSV files.

--- scripts/codingspectator-analysis/hsqldb/cleanup.sh
+++ scripts/codingspectator-analysis/hsqldb/cleanup.sh
@@ -6,6 +6,8 @@ ICSE_2012_SVN_FOLDER="$CODINGSPECTATOR_SVN_FOLDER/Experiment/Summer2011/Internal
 rm -f commands.csv
 rm -f refactoring_change_intensity.csv
 rm -f refactoringmapping.csv
+rm -f usage_time.csv
+rm -f refactoring_id_human_name_mapping.csv
 mv *.csv "$ICSE_2012_SVN_FOLDER/Results/"
 rm -f db_file.*
 rm -f *.sql
