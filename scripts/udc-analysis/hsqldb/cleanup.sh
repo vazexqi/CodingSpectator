3dbd84248f8333d41e852e5fbca4c76a2090cb4d
Mohsen Vakilian <reprogrammer@gmail.com>
2012-03-25 21:58:02 -0500

    Extracted detailed info about events related to composite refactorings.

--- scripts/udc-analysis/hsqldb/cleanup.sh
+++ scripts/udc-analysis/hsqldb/cleanup.sh
@@ -3,8 +3,11 @@
 
 COMPOSITE_REFACTORINGS_FOLDER="$CODINGSPECTATOR_SVN_FOLDER/Experiment/Summer2011/InternalAndExternal/CompositeRefactorings"
 
+tar cfz refactorings-under-study-context.csv.tar.gz refactorings-under-study-context.csv
+rm -f refactorings-under-study-context.csv
 rm -f matched-performed-refactorings.csv
 mv -f *.csv "$COMPOSITE_REFACTORINGS_FOLDER"
+mv -f *.tar.gz "$COMPOSITE_REFACTORINGS_FOLDER"
 rm -f db_file.*
 rm -f *.sql
 
