34f472759e61e56ae48e78b6072f5a59382eba15
Mohsen Vakilian <reprogrammer@gmail.com>
2012-03-26 21:02:47 -0500

    Computed the frequency and size of each refactoring.

--- scripts/udc-analysis/hsqldb/cleanup.sh
+++ scripts/udc-analysis/hsqldb/cleanup.sh
@@ -2,12 +2,17 @@
 # This file is licensed under the University of Illinois/NCSA Open Source License. See LICENSE.TXT for details.
 
 COMPOSITE_REFACTORINGS_FOLDER="$CODINGSPECTATOR_SVN_FOLDER/Experiment/Summer2011/InternalAndExternal/CompositeRefactorings"
+MAIN1_SQL=main1.sql
+MAIN2_SQL=main2.sql
 
 tar cfz refactorings-under-study-context.csv.tar.gz refactorings-under-study-context.csv
 rm -f refactorings-under-study-context.csv
 rm -f matched-performed-refactorings.csv
+rm -f transaction-patterns.csv
+rm -f detailed-transactions.csv
 mv -f *.csv "$COMPOSITE_REFACTORINGS_FOLDER"
 mv -f *.tar.gz "$COMPOSITE_REFACTORINGS_FOLDER"
 rm -f db_file.*
-rm -f *.sql
+rm -f "$MAIN1_SQL"
+rm -f "$MAIN2_SQL"
 
--- scripts/udc-analysis/hsqldb/setup.sh
+++ scripts/udc-analysis/hsqldb/setup.sh
@@ -2,17 +2,26 @@
 # This file is licensed under the University of Illinois/NCSA Open Source License. See LICENSE.TXT for details.
 
 LOGS_CSV_FOLDER="$CODINGSPECTATOR_SVN_FOLDER/Experiment/Summer2011/InternalAndExternal/ICSE2012/Data/"
-MAIN_SQL=main.sql
+MAIN1_SQL=main1.sql
+MAIN2_SQL=main2.sql
 
 tar xfz "$LOGS_CSV_FOLDER/logs-hsqldb.tar.gz" -C .
 
 ln -sf "$CODINGSPECTATOR_SVN_FOLDER/Experiment/Summer2011/InternalAndExternal/ICSE2012/Data/matched-performed-refactorings.csv" .
+ln -sf "$CODINGSPECTATOR_SVN_FOLDER/Experiment/Summer2011/InternalAndExternal/CompositeRefactorings/transaction-patterns.csv" .
+ln -sf "$CODINGSPECTATOR_SVN_FOLDER/Experiment/Summer2011/InternalAndExternal/CompositeRefactorings/detailed-transactions.csv" .
 
-echo "\c false" > "$MAIN_SQL"
+echo "\c false" > "$MAIN1_SQL"
 
 for sql_file in functions.sql export-refactorings.sql
 do
-  find "$CODINGSPECTATOR_GIT_FOLDER" -iname $sql_file -print0 | xargs -0 -I {} echo "\i "{} >> "$MAIN_SQL"
+  find "$CODINGSPECTATOR_GIT_FOLDER" -iname $sql_file -print0 | xargs -0 -I {} echo "\i "{} >> "$MAIN1_SQL"
 done
 
+echo "\c false" > "$MAIN2_SQL"
+
+for sql_file in max-lengths.sql compute-transaction-statistics.sql
+do
+  find "$CODINGSPECTATOR_GIT_FOLDER" -iname $sql_file -print0 | xargs -0 -I {} echo "\i "{} >> "$MAIN2_SQL"
+done
 
