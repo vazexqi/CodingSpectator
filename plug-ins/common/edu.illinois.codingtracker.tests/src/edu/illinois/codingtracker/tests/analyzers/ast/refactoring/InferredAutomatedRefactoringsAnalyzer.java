0a03e6182a6807f85756ece6ffd0fe92a76ad913
Wanderer777 <negara2003@yahoo.com>
2012-07-16 16:18:20 -0500

    Added an analyzer to track not inferred automated refactorings.

---
+++ plug-ins/common/edu.illinois.codingtracker.tests/src/edu/illinois/codingtracker/tests/analyzers/ast/refactoring/InferredAutomatedRefactoringsAnalyzer.java
@@ -0,0 +1,75 @@
+/**
+ * This file is licensed under the University of Illinois/NCSA Open Source License. See LICENSE.TXT for details.
+ */
+package edu.illinois.codingtracker.tests.analyzers.ast.refactoring;
+
+import edu.illinois.codingtracker.operations.UserOperation;
+import edu.illinois.codingtracker.operations.ast.InferredRefactoringOperation;
+import edu.illinois.codingtracker.operations.ast.InferredRefactoringOperation.RefactoringKind;
+import edu.illinois.codingtracker.operations.refactorings.FinishedRefactoringOperation;
+import edu.illinois.codingtracker.operations.refactorings.NewStartedRefactoringOperation;
+
+
+/**
+ * This analyzer reports potentially not inferred automated refactorings.
+ * 
+ * @author Stas Negara
+ * 
+ */
+public class InferredAutomatedRefactoringsAnalyzer extends InferredRefactoringAnalyzer {
+
+	private boolean isCurrentAutomatedRefactoringInferred= false;
+
+	private int totalAutomatedRefactoringsCount= 0;
+
+	private int totalUninferredAutomatedRefactoringsCount= 0;
+
+
+	@Override
+	protected String getTableHeader() {
+		return "username,workspace ID,version\n";
+	}
+
+	@Override
+	protected void postprocessOperation(UserOperation userOperation) {
+		if (userOperation instanceof InferredRefactoringOperation) {
+			isCurrentAutomatedRefactoringInferred= true;
+		}
+		if (userOperation instanceof NewStartedRefactoringOperation) {
+			isCurrentAutomatedRefactoringInferred= false;
+			if (getCurrentAutomatedRefactoringKind() != null) {
+				totalAutomatedRefactoringsCount++;
+			}
+		}
+		if (userOperation instanceof FinishedRefactoringOperation) {
+			if (!isCurrentAutomatedRefactoringInferred && getCurrentAutomatedRefactoringKind() != null &&
+					getCurrentAutomatedRefactoringKind() != RefactoringKind.ENCAPSULATE_FIELD &&
+					!((FinishedRefactoringOperation)userOperation).isTooSimple()) {
+				totalUninferredAutomatedRefactoringsCount++;
+				System.out.println("Timestamp: " + userOperation.getTime());
+			}
+		}
+	}
+
+	@Override
+	protected void initialize() {
+		super.initialize();
+	}
+
+	@Override
+	protected void populateResults() {
+		//no results
+	}
+
+	@Override
+	protected void finishedProcessingAllSequences() {
+		System.out.println("Total automated refactorings: " + totalAutomatedRefactoringsCount);
+		System.out.println("Total uninferred automated refactorings: " + totalUninferredAutomatedRefactoringsCount);
+	}
+
+	@Override
+	protected String getResultFilePostfix() {
+		return ".inferred_automated_refactorings";
+	}
+
+}
