86da680f1f7cb34b0745c1cdc53fe37f4ee4871f
Wanderer777 <negara2003@yahoo.com>
2012-10-06 16:19:35 -0500

    Added abstractization of the affected node's content.

--- plug-ins/common/edu.illinois.codingtracker.tests/src/edu/illinois/codingtracker/tests/postprocessors/ast/transformation/UnknownTransformationPattern.java
+++ plug-ins/common/edu.illinois.codingtracker.tests/src/edu/illinois/codingtracker/tests/postprocessors/ast/transformation/UnknownTransformationPattern.java
@@ -3,7 +3,16 @@
  */
 package edu.illinois.codingtracker.tests.postprocessors.ast.transformation;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.CharacterLiteral;
+import org.eclipse.jdt.core.dom.NumberLiteral;
+import org.eclipse.jdt.core.dom.SimpleName;
+import org.eclipse.jdt.core.dom.StringLiteral;
 
 import edu.illinois.codingtracker.operations.ast.ASTOperationDescriptor.OperationKind;
 import edu.illinois.codingtracker.operations.ast.UnknownTransformationDescriptor;
@@ -20,13 +29,67 @@ public class UnknownTransformationPattern {
 
 	private final UnknownTransformationDescriptor transformationDescriptor;
 
+	private final String abstractedNodeContent;
+
+	private int count;
+
+	private final Map<String, String> nameMapping= new HashMap<String, String>();
+
 
 	public UnknownTransformationPattern(OperationKind operationKind, ASTNode affectedNode) {
 		transformationDescriptor= new UnknownTransformationDescriptor(operationKind, getNodeType(affectedNode), affectedNode.toString(), getNodeType(affectedNode.getParent()));
+		abstractedNodeContent= getAbstractedNodeContent(affectedNode);
 	}
 
 	public UnknownTransformationDescriptor getTransformationDescriptor() {
 		return transformationDescriptor;
+	}
+
+	private String getAbstractedNodeContent(ASTNode node) {
+		resetMappings();
+		ASTNode copyNode= ASTNode.copySubtree(AST.newAST(AST.JLS3), node);
+		copyNode.accept(new ASTVisitor() {
+
+			@Override
+			public boolean visit(SimpleName node) {
+				node.setIdentifier(getAbstractName(node.getIdentifier()));
+				return true;
+			}
+
+			@Override
+			public boolean visit(StringLiteral node) {
+				node.setLiteralValue("string");
+				return true;
+			}
+
+			@Override
+			public boolean visit(CharacterLiteral node) {
+				node.setCharValue('c');
+				return true;
+			}
+
+			@Override
+			public boolean visit(NumberLiteral node) {
+				node.setToken("0");
+				return true;
+			}
+
+		});
+		return copyNode.toString();
+	}
+
+	private void resetMappings() {
+		count= 1;
+		nameMapping.clear();
+	}
+
+	private String getAbstractName(String concreteName) {
+		String abstractName= nameMapping.get(concreteName);
+		if (abstractName == null) {
+			abstractName= "id" + count++;
+			nameMapping.put(concreteName, abstractName);
+		}
+		return abstractName;
 	}
 
 	private String getNodeType(ASTNode node) {
