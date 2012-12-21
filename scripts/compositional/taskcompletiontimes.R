39d58ffa3c8bc5f744aed73a245e9f615b167bed
Mohsen Vakilian <reprogrammer@gmail.com>
2012-12-20 22:10:10 -0600

    Added a paired t-test significance test.

--- scripts/compositional/taskcompletiontimes.R
+++ scripts/compositional/taskcompletiontimes.R
@@ -36,6 +36,22 @@ run_wilcoxon <- function(a, b) {
   cat(sprintf("(W = %.2f, Z = %.2f, p = %.2f < 0.05, r = %.2f)", W, Z, p, r), "\n")
 }
 
+run_t_test <- function(a, b) {
+  cat("a =", a, "\n")
+  cat("length(a) =", length(a), "\n")
+  cat("mean(a) =", mean(a), "\n")
+  cat("sd(a) =", sd(a), "\n")
+  cat("\n")
+  cat("b =", b, "\n")
+  cat("length(b) =", length(b), "\n")
+  cat("mean(b) =", mean(b), "\n")
+  cat("sd(b) =", sd(b), "\n")
+  cat("\n")
+
+  t_test_result <- t.test(a, b, paired = TRUE, alternative = "two.sided")
+  show(t_test_result)
+}
+
 wizard_task_completion_times <- c(17, 8, 18, 19, 37, 19, 9, 12, 16, 13)
 compositional_task_completion_times <- c(9, 5, 8, 19, 11, 18, 8, 11, 18, 10)
 
@@ -43,3 +59,6 @@ cat("a = wizard_task_completion_times\n")
 cat("b = compositional_task_completion_times\n")
 cat("Wilcoxon test of task completion times:\n")
 run_wilcoxon(wizard_task_completion_times, compositional_task_completion_times)
+cat("t-test of task completion times:\n")
+run_t_test(wizard_task_completion_times, compositional_task_completion_times)
+
