dbcf4e53c86a0921600452d957ba1b3a222f9de2
Mohsen Vakilian <reprogrammer@gmail.com>
2013-02-15 21:08:02 -0600

    Fixed the license header.

--- scripts/hsqldb/run-hsqldb-mem.sh
+++ scripts/hsqldb/run-hsqldb-mem.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# This file is licensed under the University of Illinois/NCSA Open Source License. See LICENSE.TXT for details. -->
+# This file is licensed under the University of Illinois/NCSA Open Source License. See LICENSE.TXT for details.
 
 HSQLDB_FOLDER="$CODINGSPECTATOR_GIT_FOLDER/plug-ins/common/org.hsqldb/ant/downloads/hsqldb-2.2.6/hsqldb"
 HSQLDB_RC_FILE="$CODINGSPECTATOR_GIT_FOLDER/scripts/hsqldb/sqltool.rc"
