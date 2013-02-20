e3706a0f4cb59de0bcc2e5fb811735c5a147deb8
Mohsen Vakilian <reprogrammer@gmail.com>
2013-02-19 20:44:16 -0600

    Reduced the maximum heap size required for hsqldb.

--- scripts/codingspectator-analysis/hsqldb/run-hsqldb.sh
+++ scripts/codingspectator-analysis/hsqldb/run-hsqldb.sh
@@ -4,5 +4,5 @@
 HSQLDB_FOLDER="$CODINGSPECTATOR_GIT_FOLDER/plug-ins/common/org.hsqldb/ant/downloads/hsqldb-2.2.6/hsqldb"
 HSQLDB_RC_FILE="$CODINGSPECTATOR_GIT_FOLDER/plug-ins/common/edu.illinois.codingspectator.csvtosql/hsqldb/sqltool.rc"
 
-java -XX:MaxPermSize=1G -Xms1G -Xmx60G -jar "$HSQLDB_FOLDER/lib/sqltool.jar" --rcFile $HSQLDB_RC_FILE db_file
+java -XX:MaxPermSize=1G -Xms1G -Xmx30G -jar "$HSQLDB_FOLDER/lib/sqltool.jar" --rcFile $HSQLDB_RC_FILE db_file
 
